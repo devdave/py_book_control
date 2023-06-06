f4a9f4543c7d9fcfc4df74ec468bb918554b5bfc
devdave <devdave@ominian.net>
2023-06-06 17:02:04 -0600

    add modal support

--- book_control/src/main.tsx
+++ book_control/src/main.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {MantineProvider} from '@mantine/core';
+import {ModalsProvider} from '@mantine/modals';
 
 import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-       <MantineProvider withGlobalStyles withNormalizeCSS>
-            <App />
-       </MantineProvider>
-  </React.StrictMode>,
+    <React.StrictMode>
+        <MantineProvider withGlobalStyles withNormalizeCSS>
+            <ModalsProvider>
+                <App/>
+            </ModalsProvider>
+        </MantineProvider>
+    </React.StrictMode>,
 )
