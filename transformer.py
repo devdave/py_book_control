5871451e6762ca8be8455f0bf4902d73720a4856
devdave <devdave@ominian.net>
2023-06-19 17:40:13 -0600

    Commit horrible code sins to handle default parameters

--- transformer.py
+++ transformer.py
@@ -1,6 +1,8 @@
 import ast
 import pathlib
 import argparse
+import sys
+from itertools import zip_longest
 import typing as T
 
 import jinja2
@@ -22,9 +24,9 @@ class APIBridge {
 
     {% for func_name, func_def in functions|items() %}
     {%if func_def.doc %}/* {{func_def.doc}} */{% endif%}
-    async {{ func_name }}( {% for key, arg_def in func_def.args|items %}{{key}}:any, {% endfor %}) {
+    async {{ func_name }}({{func_def.compiled|join(', ')}}) {
         
-        return await this.boundary.remote("{{ func_name }}", {% for key, arg_def in func_def.args|items %}{{key}}, {% endfor %});
+        return await this.boundary.remote("{{ func_name }}", {{func_def.arg_names|join(', ')}});
     }
     {% endfor %}
 
@@ -40,12 +42,15 @@ class FuncArg(T.NamedTuple):
 
 
 class FuncDef(T.NamedTuple):
-    args: T.List[FuncArg] = []
-    doc: T.Optional[str] = ""
+    args: T.List[FuncArg]
+    doc: T.Optional[str]
+    compiled: T.List[str]
+    arg_names: T.List[str]
 
 
 def process_source(src_file: pathlib.Path, dest: pathlib.Path|None = None):
     module = ast.parse(src_file.read_text(), src_file.name, mode="exec")
+
 
     body = []
 
@@ -77,9 +82,90 @@ def process_class(class_elm: ast.ClassDef):
     return cls_name, functions
 
 
+def py2ts_value(something):
+
+    if isinstance(something, str):
+        return f"'{something}'"
+    elif isinstance(something, bool):
+        return "true" if something is True else 'false'
+    else:
+        return repr(something)
+
 def process_function(func_elm: ast.FunctionDef):
+    # unit tests... we don't need no stinking unit tests!
     # beeline for the args
-    return FuncDef(process_args(func_elm.args.args), ast.get_docstring(func_elm))
+
+    arg_map = dict()
+
+    definition = FuncDef(process_args(func_elm.args.args), ast.get_docstring(func_elm), [], [])
+
+    mapped_defaults = dict()
+
+    if len(func_elm.args.defaults) > 0:
+        names = [arg.arg for arg in func_elm.args.args]
+        names.reverse()
+        defaults = [py2ts_value(elm.value) for elm in func_elm.args.defaults]
+        defaults.reverse()
+        married = list(zip_longest(names, defaults))
+        married.reverse()
+        mapped_defaults = dict(married)
+
+
+    for arg in func_elm.args.args: # type: ast.arg
+        if arg.arg == "self": continue
+
+        definition.arg_names.append(arg.arg)
+
+        func_name = func_elm.name
+        func_type = "any"
+        arg_def = func_elm
+
+        if isinstance(arg.annotation, ast.Subscript):
+            #fuck it
+            func_type = "any"
+
+        elif arg.annotation is not None and arg.annotation.id is not None:
+            match arg.annotation.id:
+                case "str":
+                    func_type = "string"
+                case ["int","float"]:
+                    func_type = "number"
+                case "bool":
+                    func_type = "boolean"
+                case _:
+                    func_type = "any"
+
+        arg_map[arg.arg] = f"{arg.arg}:{func_type}"
+        if arg.arg not in mapped_defaults:
+            arg_body = f"{arg.arg}:{func_type}"
+        else:
+            arg_body = f"{arg.arg}:{func_type} = {mapped_defaults[arg.arg]}"
+
+        definition.compiled.append(arg_body)
+
+    return definition
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return definition
+
+
+
+
+
+
+
+
 
 
 def process_args(func_args: T.List[ast.arg]):
@@ -107,13 +193,23 @@ class MainArgs(tap.Tap):
     dest: pathlib.Path = None  # optional file to write to instead of printing to stdout
 
     def configure(self) -> None:
-        self.add_argument("source")
-        self.add_argument("dest", type=pathlib.Path, default=None)
+        self.add_argument("source", type=pathlib.Path)
+        self.add_argument("dest", type=pathlib.Path)
 
 
 
 def main():
     args = MainArgs().parse_args()
+    assert args.source.exists(), f"Cannot find source {args.source} file to process!"
+
+    if args.dest.name == "-":
+        args.dest = None
+
+    if args.dest is not None:
+        assert args.dest.parent.exists(), f"Cannot write {args.dest.name} to {args.dest.parent} as it does not exist!"
+        assert args.dest.parent.is_dir(), f"Cannot write {args.dest.name} to {args.dest.parent} as it is not a dir!"
+
+
 
     process_source(args.source, dest=args.dest)
 
