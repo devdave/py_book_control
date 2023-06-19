148a34afd77c1e0d3db098a6c9be3416e4afa892
devdave <devdave@ominian.net>
2023-06-19 17:41:22 -0600

    Add new endpoints and do some minor cleanup

--- lib/api.py
+++ lib/api.py
@@ -7,7 +7,7 @@ from . import models
 class BCAPI:
     app: BCApplication
 
-    FILE_TYPES: T.Tuple[str] = ('Database file (*.sqlite;*.sqlite3)',)
+    FILE_TYPES: T.Tuple[str] = ("Database file (*.sqlite;*.sqlite3)",)
 
     data_store: T.Dict[str, str]
 
@@ -22,27 +22,33 @@ class BCAPI:
     def alert(self, message: str):
         return self.app.main_window.create_confirmation_dialog("Problem", message)
 
-    def list_books(self):
-        return [book.asdict() for book in models.Book.Fetch_All(self.app.session)]
+    def list_books(self, stripped: bool = True):
+        return [
+            book.asdict(stripped=stripped)
+            for book in models.Book.Fetch_All(self.app.session)
+        ]
 
-    def get_current_book(self):
-        return self.app.book.asdict()
+    def get_current_book(self, stripped: bool = True):
+        return self.app.book.asdict(stripped=stripped)
 
     def set_current_book(self, book_id: str):
-        book = models.Book.Fetch_by_ID(self.app.session, book_id)
-        return book.asdict()
+        self.app.book = models.Book.Fetch_by_ID(self.app.session, book_id)
+        return self.app.book.asdict()
 
     def find_source(self):
-
-        result = self.app.main_window.create_file_dialog(webview.OPEN_DIALOG, allow_multiple=False,
-                                                         file_types=self.FILE_TYPES)
+        result = self.app.main_window.create_file_dialog(
+            webview.OPEN_DIALOG, allow_multiple=False, file_types=self.FILE_TYPES
+        )
         print(result, repr(result))
 
     def create_source(self):
-        result = self.app.main_window.create_file_dialog(webview.SAVE_DIALOG, allow_multiple=False,
-                                                         file_types=self.FILE_TYPES)
+        result = self.app.main_window.create_file_dialog(
+            webview.SAVE_DIALOG, allow_multiple=False, file_types=self.FILE_TYPES
+        )
         if result is None:
-            self.alert("No file was loaded or created, a save dialog will appear the next time you try to save.")
+            self.alert(
+                "No file was loaded or created, a save dialog will appear the next time you try to save."
+            )
 
     def fetch_chapters(self):
         return [chapter.asdict() for chapter in self.app.book.chapters]
@@ -59,11 +65,10 @@ class BCAPI:
         return True
 
     def fetch_stripped_chapters(self):
-
         return [chapter.asdict(stripped=True) for chapter in self.app.book.chapters]
 
     def create_chapter(self, chapter_name: str):
-        chapter = models.Chapter(title=chapter_name, uid=models.generate_id(7))
+        chapter = models.Chapter(title=chapter_name, uid=models.generate_id(12))
 
         self.app.book.chapters.append(chapter)
         self.app.session.add(chapter)
@@ -78,7 +83,7 @@ class BCAPI:
         scene = models.Scene.Fetch_by_uid(self.app.session, scene_uid)
         return scene.asdict()
 
-    def update_scene(self, scene_uid: str, new_data: dict):
+    def update_scene(self, scene_uid: str, new_data: T.Dict[str, str]):
         scene = models.Scene.Fetch_by_uid(self.app.session, scene_uid)
 
         scene.update(new_data)
@@ -94,12 +99,22 @@ class BCAPI:
         self.app.session.commit()
         return scene.asdict()
 
+    def delete_scene(self, chapter_uid: str, scene_uid: str):
+        scene = models.Scene.Fetch_by_uid(self.app.session, scene_uid)
+        parent = scene.chapter  # type: models.Chapter
+        parent.scenes.remove(scene)
+        self.app.session.delete(scene)
+        parent.scenes.reorder()
+        self.app.session.commit()
+
     def boot_up(self):
         """
-            Will be deprecated, automatically loads up the 1st Book for use with the app.
-                :return: bool
+        Will be deprecated, automatically loads up the 1st Book for use with the app.
+            :return: bool
         """
-        result = self.app.main_window.create_confirmation_dialog("Startup", "Click Ok to open an existing book.")
+        result = self.app.main_window.create_confirmation_dialog(
+            "Startup", "Click Ok to open an existing book."
+        )
         if result:
             # self.find_source()
             return True
