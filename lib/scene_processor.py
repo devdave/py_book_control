fe1dfd43c0423e48e1e6f25460f1a421450df19e
devdave <devdave@ominian.net>
2023-06-23 10:21:24 -0600

    switch over to use mistune for parsing
    Changed because mistune can detected
    `
    #scene 1
    This is the first para
    `
    while the other would see that as one large text block/paragraph.   Technically correct but I needed more finegrained control.

--- lib/scene_processor.py
+++ lib/scene_processor.py
@@ -1,5 +1,6 @@
 from mistletoe import Document
 from mistletoe import block_token, span_token
+import mistune
 import typing as T
 
 
@@ -17,6 +18,95 @@ def get_text(element):
             content.extend(get_text(child))
 
     return content
+
+
+class SceneProcessor2:
+    status: str
+    msg: str
+    title: T.Optional[str]
+    body: T.List[str]
+
+    parser: mistune.Markdown
+    ast: T.List[T.Dict[str, T.Any]]
+
+    def __init__(self):
+        self.status = 'error'
+        self.msg = 'unknown error or status unset'
+        self.title = None
+        self.content = []
+
+        #split vars
+        self.split_title = None
+        self.split_content = []
+
+        self.parser = mistune.create_markdown(renderer='ast')
+        self.ast = []
+
+    def collect_text(self, ast_node:T.Dict[str, T.Any]) -> str:
+        body = []
+        for child in ast_node['children']:
+            if child['type'] == 'text':
+                body.append(child['raw'])
+            elif child['type'] == 'softbreak':
+                body.append('\n')
+            else:
+                raise ValueError(f"I don't know how to collect text from {child}-{repr(child)}")
+
+        return "".join(body)
+
+    def compile(self, title, content):
+        return f"""# {title}
+
+{content}"""
+
+
+    def walk(self, raw_txt: str):
+        self.ast = self.parser(raw_txt)
+
+
+        if len(self.ast[0]) <= 0:
+            raise ValueError("A scene must have at least a title.")
+
+        if self.ast[0]['type'] != 'heading':
+            raise ValueError("A scene must start with a `# Scene title` header line.")
+
+        self.title = self.collect_text(self.ast [0]).strip()
+
+        if len(self.ast) > 1 and self.ast[1]['type'] != 'blank_line':
+            raise ValueError("Expected a blank line after the scene title!")
+
+        for child in self.ast[2:]:
+            if child['type'] == 'heading':
+                if self.status != 'split':
+                    self.status = 'split'
+                    self.split_title = self.collect_text(child).strip()
+                else:
+                    raise ValueError("Parser can only split one scene at a time!")
+
+            elif child['type'] == 'paragraph':
+                if self.status == 'split':
+                    self.split_content.append(self.collect_text(child))
+                else:
+                    self.content.append(self.collect_text(child))
+
+            elif child['type'] == 'blank_line':
+                self.content.append('')
+            else:
+                raise ValueError(f"Unexpected token {child['type']} - unsure what to do with {repr(child)}")
+
+        response = dict(
+            status=self.status,
+            title=self.title,
+            content="\n".join(self.content)
+        )
+
+        if self.status == 'split':
+            response['split_title'] = self.split_title
+            response['split_content'] = "\n".join(self.split_content)
+
+
+
+        return response
 
 
 class SceneProcessor:
@@ -96,11 +186,11 @@ class SceneProcessor:
         paragraphs = []
         for element in ast.children[1:]:
             if isinstance(
-                element,
-                (
-                    block_token.Paragraph,
-                    block_token.BlockCode,
-                ),
+                    element,
+                    (
+                            block_token.Paragraph,
+                            block_token.BlockCode,
+                    ),
             ):
                 subbody = [child.content for child in element.children]
                 paragraphs.append("\n".join(subbody))
--- tests/test_scene_processor.py
+++ tests/test_scene_processor.py
@@ -1,5 +1,6 @@
 import pytest
-from lib.scene_processor import SceneProcessor
+# from lib.scene_processor import SceneProcessor
+from lib.scene_processor import SceneProcessor2 as SceneProcessor
 
 def test_transform_md2dict():
 
@@ -10,7 +11,7 @@ def test_transform_md2dict():
 This is the scene's content!
 """
 
-    actual = processor.consume(raw)
+    actual = processor.walk(raw)
     assert actual['title'] == "Scene 1"
     assert actual['content'] == "This is the scene's content!"
 
@@ -25,10 +26,9 @@ Is this the second paragraph?
 """
 
 
+    with pytest.raises(ValueError, match="Expected a blank line after the scene title."):
+        actual = processor.walk(raw)
 
-    actual = processor.walk(raw)
-    assert actual['title'] == "Split scene"
-    assert actual['content'] == "Is this the first paragraph?\n\nIs this the second paragraph?"
 
 
 def test_catches_scene_split():
@@ -38,15 +38,15 @@ Is this the first paragraph?
 Is this the second paragraph?
 
 # Second scene    
-        """
+"""
 
     processor = SceneProcessor()
 
     response = processor.walk(raw)
 
     assert response['status'] == 'split'
-    assert response['content'] == 'Is this the first paragraph?\nIs this the second paragraph?'
-    assert response['new_title'] == 'Second scene'
+    assert response['content'] == 'Is this the first paragraph?\nIs this the second paragraph?\n'
+    assert response['split_title'] == 'Second scene'
 
 
 def test_return2md():
@@ -80,11 +80,13 @@ with a third line right after!"""
 def test_splits_2_large_paragraphs():
 
     data = """# Title 1
+
 This is a block of text that should be detected as a paragraph
 Second line of 1st paragraph of text and i need
 one more line of text to hopefully get enough material for this test
 
 # The Second title
+
 Now the test is to see if the system is smart enough to handle a block of text after the split
 point.   Ideally it should create a 'new_title' that equals 'The Secodn title' and a 'new_content'
 block that has this paragraph in it.
@@ -98,19 +100,19 @@ block that has this paragraph in it.
     content1 = """This is a block of text that should be detected as a paragraph
 Second line of 1st paragraph of text and i need
 one more line of text to hopefully get enough material for this test
-""".strip()
+
+"""
 
     title2 = 'The Second title'
     content2 = """Now the test is to see if the system is smart enough to handle a block of text after the split
 point.   Ideally it should create a 'new_title' that equals 'The Secodn title' and a 'new_content'
-block that has this paragraph in it.
-""".strip()
+block that has this paragraph in it."""
 
     assert product['title'] == title1
     assert product['content'] == content1
 
-    assert product['new_title'] == title2
-    assert product['new_content'] == content2
+    assert product['split_title'] == title2
+    assert product['split_content'] == content2
 
 
 
