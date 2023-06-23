675cb35ba98b57e0a0b5d18a0805be6733d14abb
devdave <devdave@ominian.net>
2023-06-23 15:31:16 -0600

    Quick notes added

--- lib/importer/tags_and_ns.py
+++ lib/importer/tags_and_ns.py
@@ -7,6 +7,37 @@ class NS(Enum):
     WORD_ML_20210 = "{http://schemas.microsoft.com/office/word/2010/wordml}"
 
 
+"""
+It helps to read the spec slightly intoxicated.
+
+<?xml version="1.0"?>
+<w:document xmlns:w="…">
+    <w:body>
+        <w:p>
+            <w:pPr>
+                <w:jc w:val="center"/>
+                <w:rPr>
+                    <w:i/>
+                </w:rPr>
+            </w:pPr>
+            <w:r>
+                <w:t>Hello, world.</w:t>
+            </w:r>
+        </w:p>
+    </w:body>
+</w:document>
+
+Above have 1 Paragraph, center styled with italics, and the text 'Hello, World.'
+
+
+    A document root has
+        a (B)ody
+            has one or more (P)aragraph
+                which have one more (R)uns
+                    which has a (T)ext
+"""
+
+
 class TAGS(Enum):
     Body = f"{NS.MAIN_WORD.value}body"
     ParaStyle = f"{NS.MAIN_WORD.value}pPr"
