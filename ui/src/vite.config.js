c6c3b7e2867084385d6b9f09c8f7c93ae84102bb
devdave <devdave@ominian.net>
2023-06-23 07:50:17 -0600

    Try out a couple different ways to implement path aliasing and see if they're recognized by PyCharm

---
+++ ui/src/vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { fileURLToPath } from 'node:url';
+import * as path from "path";
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react()],
+    resolve: {
+        alias: {
+            'src': fileURLToPath(new URL('./src', import.meta.url)),
+            "@": path.resolve(__dirname, "./src")
+        }
+    }
+});
