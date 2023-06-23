a1df1c5944c4c806c3b1a3d5ef99b38ee45d635b
devdave <devdave@ominian.net>
2023-06-23 07:51:17 -0600

    Switch over viewMode to the new enum type and wire in the createScene helper/manager func

--- ui/src/Book.context.tsx
+++ ui/src/Book.context.tsx
@@ -1,23 +1,24 @@
 import { createContext, useContext } from 'react'
 
-import { type Chapter, type Scene } from './types'
+import {type Chapter, type Scene, ViewModes} from './types'
 import APIBridge from "./lib/remote";
 
 interface BookContextValue {
   activeChapter: Chapter | undefined
   activeScene: Scene | undefined
   chapters: Chapter[] | undefined
-  viewMode: string
+  viewMode: ViewModes
   api: APIBridge
   addChapter(): void
   addScene(chapterId: string): void
+  createScene(chapterId: string, sceneTitle: string, order?: number): void
   reorderChapter(from: number, to: number): void
   reorderScene(chapterId: string, from: number, to: number): void
   setActiveChapter(chapter: Chapter): void
   setActiveScene(chapter: Chapter, scene: Scene): void
   updateChapter(chapter: Chapter): void
   updateScene(scene: Scene): void
-  setViewMode(mode: string): void
+  setViewMode(mode: ViewModes): void
 }
 
 // @ts-ignore
