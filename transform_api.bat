8bab30f492a268d6112801cd99f063c252bc069c
devdave <devdave@ominian.net>
2023-06-29 10:33:18 -0600

    cleanup trash

--- transform_api.bat
+++
@@ -1,3 +0,0 @@
-echo "Transforming API"
-python ./transformer.py lib/api.py > book_control/src/bridge.ts
-echo "Done!"
--- transform_api_infin.bat
+++
@@ -1,3 +0,0 @@
-echo "Transforming API"
-python ./transformer.py lib/api.py > infinui/src/lib/api_bridge.ts
-echo "Done!"
