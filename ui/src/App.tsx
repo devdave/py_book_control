8841b18fc660caf60b471288d3abae8c1a1f041b
devdave <devdave@ominian.net>
2023-07-05 09:21:01 -0600

    Stub for visual font selection logic

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -22,6 +22,8 @@ export default function App() {
     const [bookTitle, setBookTitle] = useState(undefined);
     const [bookId, setBookId] = useState(undefined);
 
+    const [fonts, setFonts] = useState<string[]>([]);
+    const [activeFont, setActiveFont] = useState<string|undefined>(undefined);
 
     const boundary = new Boundary()
     const api = new APIBridge(boundary);
@@ -39,6 +41,39 @@ export default function App() {
 
     }
 
+    const listFonts = (fontFaceSet:FontFaceSet):string[] => {
+        let { fonts } = document;
+        const iter = fontFaceSet.values()
+
+        let collect = new Set<string>();
+        let done = false;
+        for (const fontFace of fontFaceSet.values()) {
+            console.log(fontFace);
+        }
+
+        return [...collect.values()];
+    }
+
+    const checkFonts = () => {
+        const fontCheck = new Set([
+              // Windows 10
+            'Arial', 'Arial Black', 'Bahnschrift', 'Calibri', 'Cambria', 'Cambria Math', 'Candara', 'Comic Sans MS', 'Consolas', 'Constantia', 'Corbel', 'Courier New', 'Ebrima', 'Franklin Gothic Medium', 'Gabriola', 'Gadugi', 'Georgia', 'HoloLens MDL2 Assets', 'Impact', 'Ink Free', 'Javanese Text', 'Leelawadee UI', 'Lucida Console', 'Lucida Sans Unicode', 'Malgun Gothic', 'Marlett', 'Microsoft Himalaya', 'Microsoft JhengHei', 'Microsoft New Tai Lue', 'Microsoft PhagsPa', 'Microsoft Sans Serif', 'Microsoft Tai Le', 'Microsoft YaHei', 'Microsoft Yi Baiti', 'MingLiU-ExtB', 'Mongolian Baiti', 'MS Gothic', 'MV Boli', 'Myanmar Text', 'Nirmala UI', 'Palatino Linotype', 'Segoe MDL2 Assets', 'Segoe Print', 'Segoe Script', 'Segoe UI', 'Segoe UI Historic', 'Segoe UI Emoji', 'Segoe UI Symbol', 'SimSun', 'Sitka', 'Sylfaen', 'Symbol', 'Tahoma', 'Times New Roman', 'Trebuchet MS', 'Verdana', 'Webdings', 'Wingdings', 'Yu Gothic',
+              // macOS
+              'American Typewriter', 'Andale Mono', 'Arial', 'Arial Black', 'Arial Narrow', 'Arial Rounded MT Bold', 'Arial Unicode MS', 'Avenir', 'Avenir Next', 'Avenir Next Condensed', 'Baskerville', 'Big Caslon', 'Bodoni 72', 'Bodoni 72 Oldstyle', 'Bodoni 72 Smallcaps', 'Bradley Hand', 'Brush Script MT', 'Chalkboard', 'Chalkboard SE', 'Chalkduster', 'Charter', 'Cochin', 'Comic Sans MS', 'Copperplate', 'Courier', 'Courier New', 'Didot', 'DIN Alternate', 'DIN Condensed', 'Futura', 'Geneva', 'Georgia', 'Gill Sans', 'Helvetica', 'Helvetica Neue', 'Herculanum', 'Hoefler Text', 'Impact', 'Lucida Grande', 'Luminari', 'Marker Felt', 'Menlo', 'Microsoft Sans Serif', 'Monaco', 'Noteworthy', 'Optima', 'Palatino', 'Papyrus', 'Phosphate', 'Rockwell', 'Savoye LET', 'SignPainter', 'Skia', 'Snell Roundhand', 'Tahoma', 'Times', 'Times New Roman', 'Trattatello', 'Trebuchet MS', 'Verdana', 'Zapfino',
+            ].sort());
+
+        const fontAvailable = new Set();
+
+      for (const font of fontCheck.values()) {
+        if (document.fonts.check(`12px "${font}"`)) {
+          fontAvailable.add(font);
+        }
+      }
+
+        console.log('Available Fonts:', [...fontAvailable.values()]);
+    }
+
+
     useEffect(() => {
         //@ts-ignore Fuck the hell off with this window not defined shit
         if (window.pywebview !== undefined && window.pywebview.api !== undefined) {
@@ -46,17 +81,22 @@ export default function App() {
         } else {
             window.addEventListener(PYWEBVIEWREADY, doReady);
         }
-    });
+    },[]);
 
-    if(isReady === false){
+    useEffect(()=>{
+
+        document.fonts.ready.then((fontFaceSet)=>{
+            checkFonts();
+        })
+
+    },[]);
+
+
+    if(!isReady){
         return (
             <LoadingOverlay visible={true}/>
         )
     }
-
-
-
-
 
 
     //Next step would be to show a Modal list "Use last book", "select another book", "Create a book"
