e7f9f4406c84e8a89bc1d86837d99e2fc8c13ae8
devdave <devdave@ominian.net>
2023-07-10 20:40:00 -0600

    reformatted

--- ui/src/widget/ResizeablePanels.tsx
+++ ui/src/widget/ResizeablePanels.tsx
@@ -1,130 +1,129 @@
 import { Divider, Flex } from '@mantine/core'
-import { createRef, FC, MouseEventHandler, useEffect, useRef, useState } from 'react'
+import { createRef, FC, MouseEventHandler, useEffect, useRef, useState, ReactNode, Children } from 'react'
 
-import { useViewportSize } from '@mantine/hooks'
+// import { useViewportSize } from '@mantine/hooks'
 
-const LeftPanel: React.FC<{
-  leftWidth: number | undefined
-  setLeftWidth: (value: number) => void
-  startingWidth: number
-  minWidth: number
-  children?: React.ReactNode
+const LeftPanel: FC<{
+    leftWidth: number | undefined
+    setLeftWidth: (value: number) => void
+    minWidth: number
+    children?: React.ReactNode
 }> = ({ leftWidth, setLeftWidth, minWidth, children }) => {
-  const leftRef = createRef<HTMLDivElement>()
+    const leftRef = createRef<HTMLDivElement>()
 
-  useEffect(() => {
-    if (leftRef.current) {
-      if (!leftWidth) {
-        setLeftWidth(Math.max(leftRef.current?.clientWidth, 1000))
-      }
-      const gatedWidth = Math.max(leftWidth as number, minWidth)
+    useEffect(() => {
+        if (leftRef.current) {
+            if (!leftWidth) {
+                setLeftWidth(Math.max(leftRef.current?.clientWidth, 1000))
+            }
+            const gatedWidth = Math.max(leftWidth as number, minWidth)
 
-      leftRef.current.style.width = `${gatedWidth}px`
-    }
-  }, [leftRef, leftWidth, setLeftWidth])
+            leftRef.current.style.width = `${gatedWidth}px`
+        }
+    }, [leftRef, leftWidth, setLeftWidth])
 
-  return (
-    <div
-      ref={leftRef}
-      style={{
-        height: '100%',
-        width: '100%',
-        boxSizing: 'border-box'
-      }}
-    >
-      {children}
-    </div>
-  )
+    return (
+        <div
+            ref={leftRef}
+            style={{
+                height: '100%',
+                width: '100%',
+                boxSizing: 'border-box'
+            }}
+        >
+            {children}
+        </div>
+    )
 }
 
 interface ResizeablePanelsProps {
-  children: React.ReactNode[]
+    children: ReactNode[]
 }
 
-export const ResizeablePanels: React.FC<ResizeablePanelsProps> = ({ children }) => {
-  const dividerRef = createRef<HTMLDivElement>()
-  const rightPanelRef = createRef<HTMLDivElement>()
+export const ResizeablePanels: FC<ResizeablePanelsProps> = ({ children }) => {
+    const dividerRef = createRef<HTMLDivElement>()
+    const rightPanelRef = createRef<HTMLDivElement>()
 
-  const [dragging, setDragging] = useState(false)
-  const [leftWidth, setLeftWidth] = useState<undefined | number>(undefined)
-  const [separatorXPosition, setSeparatorXPosition] = useState<undefined | number>(undefined)
-  const { width: vpWidth } = useViewportSize()
+    const [dragging, setDragging] = useState(false)
+    const [leftWidth, setLeftWidth] = useState<undefined | number>(undefined)
+    const [separatorXPosition, setSeparatorXPosition] = useState<undefined | number>(undefined)
+    // const { width: vpWidth } = useViewportSize()
 
-  //@ts-ignore No seriously, shut the fuck up
-  useEffect(() => {
-    //@ts-ignore
-    document.addEventListener('mousemove', onMouseMove)
-    //@ts-ignore
-    document.addEventListener('mouseup', onMouseUp)
-    return () => {
-      //@ts-ignore
-      document.removeEventListener('mousemove', onMouseMove)
-      //@ts-ignore
-      document.addEventListener('mouseup', onMouseUp)
+    //@ts-ignore No seriously, shut the fuck up
+    useEffect(() => {
+        //@ts-ignore
+        document.addEventListener('mousemove', onMouseMove)
+        //@ts-ignore
+        document.addEventListener('mouseup', onMouseUp)
+        return () => {
+            //@ts-ignore
+            document.removeEventListener('mousemove', onMouseMove)
+            //@ts-ignore
+            document.addEventListener('mouseup', onMouseUp)
+        }
+    })
+
+    if (Children.count(children) !== 2) {
+        throw Error('Resizeable takes only 2 children.')
     }
-  })
 
-  if (children.length != 2) {
-    throw Error('Resizeable takes only 2 children.')
-  }
+    const left = children[0]
+    const right = children[1]
+    // const startWidth = vpWidth * 0.7
+    const minWidth = 300
 
-  const left = children[0]
-  const right = children[1]
-  const startWidth = vpWidth * 0.7
-  const minWidth = 300
-
-  const onMouseDown: MouseEventHandler = (evt) => {
-    setDragging(true)
-    setSeparatorXPosition(evt.clientX)
-  }
-
-  const onMouseUp: MouseEventHandler = () => {
-    setDragging(false)
-  }
-
-  const onMouseMove: MouseEventHandler = (evt) => {
-    if (dragging && leftWidth && separatorXPosition) {
-      const newLeftWidth = leftWidth + evt.clientX - separatorXPosition
-      setSeparatorXPosition(evt.clientX)
-      setLeftWidth(newLeftWidth)
+    const onMouseDown: MouseEventHandler = (evt) => {
+        setDragging(true)
+        setSeparatorXPosition(evt.clientX)
     }
-  }
 
-  return (
-    <Flex
-      id='resizeableContainer'
-      mih={50}
-      gap='md'
-      justify='center'
-      align='flex-start'
-      direction='row'
-      wrap='nowrap'
-      style={{
-        height: '100%'
-      }}
-    >
-      <LeftPanel
-        leftWidth={leftWidth}
-        setLeftWidth={setLeftWidth}
-        startingWidth={startWidth}
-        minWidth={minWidth}
-      >
-        {left}
-      </LeftPanel>
-      <Divider
-        size='xl'
-        ref={dividerRef}
-        orientation='vertical'
-        onMouseDown={onMouseDown}
-        onMouseUp={onMouseUp}
-        style={{ cursor: 'grab', padding: '0 6px 0 6px' }}
-      />
-      <div
-        ref={rightPanelRef}
-        style={{ flex: 1, height: '100%', position: 'relative', minWidth: '14rem' }}
-      >
-        {right}
-      </div>
-    </Flex>
-  )
+    const onMouseUp: MouseEventHandler = () => {
+        setDragging(false)
+    }
+
+    const onMouseMove: MouseEventHandler = (evt) => {
+        if (dragging && leftWidth && separatorXPosition) {
+            const newLeftWidth = leftWidth + evt.clientX - separatorXPosition
+            setSeparatorXPosition(evt.clientX)
+            setLeftWidth(newLeftWidth)
+        }
+    }
+
+    return (
+        <Flex
+            id='resizeableContainer'
+            mih={50}
+            gap='md'
+            justify='center'
+            align='flex-start'
+            direction='row'
+            wrap='nowrap'
+            style={{
+                height: '100%'
+            }}
+        >
+            <LeftPanel
+                leftWidth={leftWidth}
+                setLeftWidth={setLeftWidth}
+                // startingWidth={startWidth}
+                minWidth={minWidth}
+            >
+                {left}
+            </LeftPanel>
+            <Divider
+                size='xl'
+                ref={dividerRef}
+                orientation='vertical'
+                onMouseDown={onMouseDown}
+                onMouseUp={onMouseUp}
+                style={{ cursor: 'grab', padding: '0 6px 0 6px' }}
+            />
+            <div
+                ref={rightPanelRef}
+                style={{ flex: 1, height: '100%', position: 'relative', minWidth: '14rem' }}
+            >
+                {right}
+            </div>
+        </Flex>
+    )
 }
