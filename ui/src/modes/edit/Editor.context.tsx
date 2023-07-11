c8581b2320e6f3b063fa9ded55445aa35a2caf4e
devdave <devdave@ominian.net>
2023-07-10 19:53:51 -0600

    Prettiered and fixed.   Removed fetchScene as it is broken

--- ui/src/modes/edit/Editor.context.tsx
+++ ui/src/modes/edit/Editor.context.tsx
@@ -1,31 +1,30 @@
 import { createContext, useContext } from 'react'
 
-import {type Book, type Chapter, type ChapterIndex, EditModes, type Scene, type SceneIndex} from '@src/types'
-import APIBridge from "@src/lib/remote";
+import { type Book, type Chapter, type ChapterIndex, EditModes, type Scene, type SceneIndex } from '@src/types'
+import APIBridge from '@src/lib/remote'
 
 export interface EditorContextValue {
-  index: Chapter[]
-  activeBook: Book
-  activeChapter: Chapter | ChapterIndex | undefined
-  activeScene: Scene | SceneIndex | undefined
-  chapters: Chapter[] | ChapterIndex[] | undefined
-  editMode: EditModes
-  api: APIBridge
-  addChapter(): void
-  addScene(chapterId: string): Promise<Scene|void>
-  createScene(chapterId: string, sceneTitle: string, order?: number, content?: string): Promise<void>
-  reorderChapter(from: number, to: number): void
-  reorderScene(chapterId: string, from: number, to: number): void
-  setActiveChapter(chapter: Chapter|ChapterIndex|undefined): void
-  setActiveScene(chapter: ChapterIndex | Chapter | undefined, scene: SceneIndex | Scene | undefined): void
-  updateChapter(chapter: Chapter): void
-  updateScene(scene: Partial<Scene>): void
-  deleteScene(chapterId: string, sceneId: string): void
-  fetchScene(sceneId: string): Promise<Scene>
-  setEditMode(mode: EditModes): void
-  _setChapters (chapters: Chapter[]):void,
-  _setActiveChapter (chapter: Chapter|ChapterIndex|undefined): void,
-  _setActiveScene(scene: Scene|SceneIndex|undefined): void,
+    index: Chapter[]
+    activeBook: Book
+    activeChapter: Chapter | ChapterIndex | undefined
+    activeScene: Scene | SceneIndex | undefined
+    chapters: Chapter[] | ChapterIndex[] | undefined
+    editMode: EditModes
+    api: APIBridge
+    addChapter(): void
+    addScene(chapterId: string): Promise<Scene | void>
+    createScene(chapterId: string, sceneTitle: string, order?: number, content?: string): Promise<void>
+    reorderChapter(from: number, to: number): void
+    reorderScene(chapterId: string, from: number, to: number): void
+    setActiveChapter(chapter: Chapter | ChapterIndex | undefined): void
+    setActiveScene(chapter: ChapterIndex | Chapter | undefined, scene: SceneIndex | Scene | undefined): void
+    updateChapter(chapter: Chapter): void
+    updateScene(scene: Partial<Scene>): void
+    deleteScene(chapterId: string, sceneId: string): void
+    setEditMode(mode: EditModes): void
+    _setChapters(chapters: Chapter[]): void
+    _setActiveChapter(chapter: Chapter | ChapterIndex | undefined): void
+    _setActiveScene(scene: Scene | SceneIndex | undefined): void
 }
 
 // @ts-ignore
