69ae2f2b9d163d81f41092a64fc61fc32bfa897a
devdave <devdave@ominian.net>
2023-07-10 21:27:48 -0600

    A bit of prettier cleanup

--- ui/src/modes/edit/Body.tsx
+++ ui/src/modes/edit/Body.tsx
@@ -1,37 +1,29 @@
-import { EditModes } from '@src/types';
-import { RightPanel } from '@src/modes/edit/editor_panel/RightPanel';
-import { ContinuousBody } from '@src/modes/edit/continuous_panel';
-import { useEditorContext } from '@src/modes/edit/Editor.context';
-import { Text } from '@mantine/core';
+import { EditModes } from '@src/types'
+import { RightPanel } from '@src/modes/edit/editor_panel/RightPanel'
+import { ContinuousBody } from '@src/modes/edit/continuous_panel'
+import { useEditorContext } from '@src/modes/edit/Editor.context'
+import { Text } from '@mantine/core'
 
 export const Body = () => {
     const { editMode, activeChapter, activeScene } = useEditorContext()
 
+    console.log('Body ', editMode, activeChapter, activeScene)
+
     switch (editMode) {
         case EditModes.LIST:
             if (activeChapter !== undefined) {
-                return (
-                    <RightPanel key={`${activeChapter.updated_on} ${activeChapter.id}-${activeScene?.id}`} />
-                )
+                return <RightPanel key={`${activeChapter.updated_on} ${activeChapter.id}-${activeScene?.id}`} />
             }
-                return (
-                    <h2>Create a new chapter!</h2>
-                )
+            return <h2>Create a new chapter!</h2>
 
-            break;
+            break
         case EditModes.FLOW:
             if (activeChapter !== undefined) {
-                return (
-                    <ContinuousBody key={`${activeChapter.updated_on} ${activeChapter.id}`} />
-                )
+                return <ContinuousBody key={`${activeChapter.updated_on} ${activeChapter.id}`} />
             }
-                return (
-                    <h2>Create a new chapter!</h2>
-                )
+            return <h2>Create a new chapter!</h2>
 
         default:
-            return (
-                <Text>Error: unknown edit mode {editMode}</Text>
-            )
+            return <Text>Error: unknown edit mode {editMode}</Text>
     }
 }
