04f8518fc6fb59dd3cc50242c579dd8f919c309a
devdave <devdave@ominian.net>
2023-07-16 10:45:05 -0600

    Fleshed out detail view for toons

---
+++ ui/src/modes/edit/character_panel/CharacterDetail.tsx
@@ -0,0 +1,97 @@
+import React, { useEffect } from 'react'
+import { useQuery } from '@tanstack/react-query'
+import { useAppContext } from '@src/App.context'
+import { Character } from '@src/types'
+import {
+    Box,
+    createStyles,
+    LoadingOverlay,
+    Skeleton,
+    Tabs,
+    Textarea,
+    TextInput,
+    Text
+} from '@mantine/core'
+import { useForm } from '@mantine/form'
+import { IndicatedTextInput } from '@src/widget/IndicatedTextInput'
+import { IndicatedTextarea } from '@src/widget/IndicatedTextarea'
+import { useDebouncedEffect } from '@src/lib/useDebouncedEffect'
+
+const useStyle = createStyles((theme) => ({
+    filled_textarea: {
+        height: '100%',
+        width: '100%',
+        boxSizing: 'border-box'
+    }
+}))
+
+interface FormProps {
+    name: string
+    notes: string
+}
+
+interface CharacterDetailProps {
+    character: Character
+}
+
+export const CharacterDetail: React.FC<CharacterDetailProps> = ({ character }) => {
+    const { api, activeBook, debounceTime } = useAppContext()
+
+    const { classes } = useStyle()
+
+    const form = useForm<FormProps>({
+        initialValues: {
+            name: character.name,
+            notes: character.notes
+        }
+    })
+
+    useDebouncedEffect(() => {}, [form.values], { delay: debounceTime })
+
+    console.log('Showing ', character)
+
+    return (
+        <Tabs
+            defaultValue='notes'
+            className={classes.filled_textarea}
+            style={{
+                minWidth: '100%'
+            }}
+        >
+            <Tabs.List>
+                <Tabs.Tab value='notes'>Notes</Tabs.Tab>
+                <Tabs.Tab value='Scenes'>Scenes present</Tabs.Tab>
+            </Tabs.List>
+            <Tabs.Panel
+                value='notes'
+                style={{ width: '100%', minWidth: '100%' }}
+            >
+                <IndicatedTextInput
+                    form={form}
+                    fieldName='name'
+                    label='Name'
+                    containerprops={{ miw: '100%' }}
+                    indicatorprops={{ miw: '100%' }}
+                    inputprops={{ maw: '35%' }}
+                />
+                <Text>Notes</Text>
+                <IndicatedTextarea
+                    form={form}
+                    formField='notes'
+                    inputProps={{
+                        classNames: {
+                            input: classes.filled_textarea,
+                            root: classes.filled_textarea,
+                            wrapper: classes.filled_textarea
+                        },
+                        style: {
+                            width: '100%',
+                            minWidth: '100%'
+                        },
+                        minRows: 5
+                    }}
+                />
+            </Tabs.Panel>
+        </Tabs>
+    )
+}
