cd8ee5ea71af8553a66eed8b803916816cf338ea
devdave <devdave@ominian.net>
2023-07-17 17:21:36 -0600

    extended line width

--- ui/.eslintrc.js
+++ ui/.eslintrc.js
@@ -11,10 +11,7 @@ module.exports = {
         project: './tsconfig.json'
     },
     rules: {
-        'no-param-reassign': [
-            2,
-            { props: true, ignorePropertyModificationsFor: ['draft'] }
-        ],
+        'no-param-reassign': [2, { props: true, ignorePropertyModificationsFor: ['draft'] }],
         'react/prop-types': 0,
         // indent: ['error', 4],
         'react/jsx-indent-props': ['error', 4],
@@ -23,7 +20,7 @@ module.exports = {
         '@typescript-eslint/space-before-function-paren': 'off',
         'comma-dangle': 'off',
         'jsx-quotes': ['error', 'prefer-single'],
-        'max-len': ['warn', 90],
+        'max-len': ['warn', 110],
         'no-console': 'off',
         'react-hooks/exhaustive-deps': 'warn',
         'react/react-in-jsx-scope': 'off',
