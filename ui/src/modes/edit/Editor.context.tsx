49eac203151fc0568f6e23d172bbbbdc349ad923
devdave <devdave@ominian.net>
2023-07-18 10:16:21 -0600

    Add new endpoints and rearrange be category

--- ui/src/modes/edit/Editor.context.tsx
+++ ui/src/modes/edit/Editor.context.tsx
@@ -9,31 +9,40 @@ import {
     Character,
     EditModes,
     type Scene,
-    type SceneIndex
+    type SceneIndex,
+    UID
 } from '@src/types'
 import APIBridge from '@src/lib/remote'
 import { Updater } from 'use-immer'
 import { ActiveElementHelper } from '@src/lib/ActiveElementHelper'
 
 export interface EditorContextValue {
+    api: APIBridge
+
     index: Chapter[]
+    activeElement: ActiveElementHelper
     activeChapter: Chapter | ChapterIndex | undefined
     activeScene: Scene | SceneIndex | undefined
+
     editMode: EditModes
-    api: APIBridge
-    addChapter(): void
-    addScene(chapterId: string): Promise<Scene | void>
-    createScene(chapterId: string, sceneTitle: string, order?: number, content?: string): Promise<void>
-    reorderChapter(from: number, to: number): void
-    reorderScene(chapterId: string, from: number, to: number): void
+    setEditMode(mode: EditModes): void
+
     setActiveChapter(chapter: Chapter | ChapterIndex | undefined): void
     setActiveScene(chapter: ChapterIndex | Chapter | undefined, scene: SceneIndex | Scene | undefined): void
+
+    fetchChapter(book_id: UID, chapter_id: UID): UseQueryResult<Chapter, Error>
+    addChapter(): void
     updateChapter(chapter: Chapter): void
+    reorderChapter(from: number, to: number): void
+
+    fetchScene(chapter_id: UID, scene_id: UID): UseQueryResult<Scene, Error>
+    addScene(chapterId: string): Promise<Scene | void>
+    createScene(chapterId: string, sceneTitle: string, order?: number, content?: string): Promise<void>
+    reorderScene(chapterId: string, from: number, to: number): void
     updateScene(scene: Partial<Scene>): void
     deleteScene(chapterId: string, sceneId: string): void
-    setEditMode(mode: EditModes): void
+
     changeBookTitle: UseMutationResult<Book, Error, string>
-    activeElement: ActiveElementHelper
 
     fetchCharacter(book_id: string, character_id: string, enabled: boolean): UseQueryResult<Character, Error>
     updateCharacter(changeset: Character): void
