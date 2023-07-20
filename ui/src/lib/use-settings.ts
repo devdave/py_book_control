97a10f2ea9c7b0ec145292aedd932b2f9145c3dc
devdave <devdave@ominian.net>
2023-07-20 16:50:35 -0600

    Seriously I need to stop fucking with this

--- ui/src/lib/use-settings.ts
+++ ui/src/lib/use-settings.ts
@@ -1,16 +1,24 @@
 import { map, Dictionary } from 'lodash'
 import { UseMutationResult, UseQueryResult } from '@tanstack/react-query'
 import { useCallback } from 'react'
+import { useImmer } from 'use-immer'
+import { object, string, unknown } from 'zod'
 
 type GenericTypes<TContainer> = TContainer[keyof TContainer]
 
 export interface SettingsManagerReturn<TValues> {
     get: <Field extends keyof TValues>(name: Field) => UseQueryResult<TValues[Field]> | undefined
     set: <Field extends keyof TValues>(name: Field, value: TValues[Field]) => TValues[Field]
-    reconcile: () => void
+    reconcile: (onValuesLoaded?: (values: NonNullable<unknown>[]) => void) => void
     makeState: <Field extends keyof TValues>(
         name: Field
     ) => [TValues[Field] | undefined, boolean, (value: TValues[Field]) => void]
+}
+
+export interface ApplicationSetting<TValues extends object = Record<string, unknown>> {
+    name: keyof TValues
+    value: TValues[keyof TValues]
+    type: 'string' | 'boolean' | 'number' | 'undefined'
 }
 
 export function useSettings<TValues extends object = Record<string, unknown>>({
@@ -21,10 +29,10 @@ export function useSettings<TValues extends object = Record<string, unknown>>({
     bulkDefaultSetter
 }: {
     defaultSettings: TValues
-    bulkFetchSettings: () => Promise<object>
+    bulkFetchSettings: () => Promise<ApplicationSetting<TValues>[]>
     setter: UseMutationResult<undefined, unknown, { name: string; value: string }, unknown>
     getter: (<Field extends keyof TValues>(name: Field) => UseQueryResult<TValues[Field]>) | undefined
-    bulkDefaultSetter: (changeset: object[]) => Promise<object>
+    bulkDefaultSetter: (changeset: ApplicationSetting<TValues>[]) => Promise<object>
 }): SettingsManagerReturn<TValues> {
     /**
      * Internalized getter
@@ -34,6 +42,7 @@ export function useSettings<TValues extends object = Record<string, unknown>>({
     const get = useCallback(
         <Field extends keyof TValues>(name: Field): UseQueryResult<TValues[Field]> | undefined =>
             getter ? getter(name) : undefined,
+
         [getter]
     )
 
@@ -52,7 +61,6 @@ export function useSettings<TValues extends object = Record<string, unknown>>({
                 setter.mutate({ name: coerce_name, value: coerce_value })
             }
 
-            // eslint-disable-next-line no-param-reassign
             return value
         },
         [setter]
@@ -82,26 +90,53 @@ export function useSettings<TValues extends object = Record<string, unknown>>({
      * Must be called before any other member function is used
      *
      */
-    const reconcile = useCallback(() => {
-        console.log('Ensuring defaults are set')
-        const changeset = map(
-            defaultSettings as Dictionary<unknown>,
-            (
-                name: keyof TValues,
-                value: GenericTypes<TValues>
-            ): { name: keyof TValues; value: GenericTypes<TValues>; type: string } => ({
-                name,
-                value,
-                type: typeof defaultSettings[name] as string
-            })
-        )
-        console.log(`Defaults are ${JSON.stringify(changeset)}`)
+    const reconcile = useCallback(
+        (onValuesLoaded: undefined | ((changeset: ApplicationSetting<TValues>[]) => void) = undefined) => {
+            console.log('Ensuring defaults are set')
+            if (!defaultSettings) {
+                throw new Error('Somehow missing default settings!  Cannot proceed')
+            }
 
-        const coerced = changeset as unknown
-        bulkDefaultSetter(coerced as Array<object>)
-            .then(() => console.log('Defaults set'))
-            .catch((reason) => console.error('Defaults failed with', reason))
-    }, [bulkDefaultSetter, defaultSettings])
+            const changeset: { [key: string]: string | any }[] = []
+
+            // eslint-disable-next-line no-restricted-syntax
+            for (const [key, value] of Object.entries(defaultSettings)) {
+                changeset.push({
+                    name: key,
+                    value,
+                    type: typeof value
+                })
+            }
+
+            /**
+            const changeset: ApplicationSetting[] = map<TValues, ApplicationSetting>(
+                Object.entries(defaultSettings),
+                (
+                    value: GenericTypes<TValues>,
+                    name: keyof TValues
+                ): { name: keyof TValues; value: GenericTypes<TValues>; type: string } => {
+                    console.log('def', name, value)
+                    return {
+                        name,
+                        value,
+                        type: typeof defaultSettings[name] as string
+                    }
+                }
+            )*/
+            console.log(`Defaults are ${JSON.stringify(changeset)}`)
+            const coerced = changeset as ApplicationSetting<TValues>[]
+            bulkDefaultSetter(coerced)
+                .then(() => console.log('Defaults set'))
+                .catch((reason) => console.error('Defaults failed with', reason))
+
+            if (onValuesLoaded) {
+                bulkFetchSettings().then((payload: ApplicationSetting<TValues>[]) => {
+                    onValuesLoaded(payload)
+                })
+            }
+        },
+        [bulkDefaultSetter, bulkFetchSettings, defaultSettings]
+    )
 
     return { get, set, reconcile, makeState }
 }
@@ -111,38 +146,53 @@ export function useSettings<TValues extends object = Record<string, unknown>>({
  *
  */
 
-// interface TestSettings {
-//     delay: number
-//     disableSomethingImportant: boolean
-//     thingName: string
-// }
-//
-// const settings = useSettings<TestSettings>({
-//     getter: undefined,
-//     setter: undefined,
-//     defaultSetter: undefined,
-//     defaultSettings: {
-//         delay: 123,
-//         disableSomethingImportant: false,
-//         thingName: 'igor'
-//     }
-// })
-//
-// // call when api bridge is up
-// settings.reconcile()
-//
-// const [thingName, thingIsLoading, setThingName] = settings.makeState('thingName')
-// const [delay, delayIsLoading, setDelay] = settings.makeState('delay')
-// const [toggle1, toggleIsLoading, setToggle1] = settings.makeState('disableSomethingImportant')
-//
-// if (thingIsLoading || delayIsLoading || toggleIsLoading) {
-//     console.log('Stuff is still loading!')
-// }
-//
-// console.log('thing is ', thingName)
-// console.log('delay is', delay)
-// console.log('toggle1 is', toggle1)
-//
-// setThingName('Bob')
-// setDelay(856)
-// setToggle1(!toggle1)
+/**
+
+ // Requires a type interface
+interface TestSettings {
+    delay: number
+    disableSomethingImportant: boolean
+    thingName: string
+}
+
+const settings = useSettings<TestSettings>({
+    getter: (settingName)=>api.fetchSetting(settingName),
+    setter: (settingName, settingValue)=>api.setSetting(settingName settingValue),
+    defaultSetter: undefined,
+    defaultSettings: {
+        delay: 123,
+        disableSomethingImportant: false,
+        thingName: 'igor'
+    }
+})
+
+ const = onValuesFetched(values:TestSettings) => {
+    forEach(values, (name, value)=>{
+        // For cases like react query when you don't want to spam the hell
+        // out of the backend
+        localApi.addToSettingsCache(name, value)
+    })
+    //For a scenario where the application is waiting to proceed
+    setAppSettingAreReady(true)
+ }
+
+// call when api bridge is up
+settings.reconcile()
+
+const [thingName, thingIsLoading, setThingName] = settings.makeState('thingName')
+const [delay, delayIsLoading, setDelay] = settings.makeState('delay')
+const [toggle1, toggleIsLoading, setToggle1] = settings.makeState('disableSomethingImportant')
+
+if (thingIsLoading || delayIsLoading || toggleIsLoading) {
+    console.log('Stuff is still loading!')
+}
+
+console.log('thing is ', thingName)
+console.log('delay is', delay)
+console.log('toggle1 is', toggle1)
+
+setThingName('Bob')
+setDelay(856)
+setToggle1(!toggle1)
+
+ */
