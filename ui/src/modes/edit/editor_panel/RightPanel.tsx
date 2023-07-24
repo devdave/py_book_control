0d08ed68be626fc9cb991bb45b296d0e4426c997
devdave <devdave@ominian.net>
2023-07-24 16:56:02 -0600

    Fixed up create scene a little bit more

--- ui/src/modes/edit/editor_panel/RightPanel.tsx
+++ ui/src/modes/edit/editor_panel/RightPanel.tsx
@@ -3,18 +3,30 @@ import { IconPlus } from '@tabler/icons-react'
 import { type FC, useCallback } from 'react'
 
 import { Chapter } from '@src/types'
+import { InputModal } from '@src/widget/input_modal'
 import { useEditorContext } from '../Editor.context'
 import { ChapterForm } from './ChapterForm'
 
 import SceneList from './SceneList'
 
 export const RightPanel: FC = () => {
-    const { activeChapter, sceneBroker } = useEditorContext()
+    const { activeChapter, setActiveScene, sceneBroker } = useEditorContext()
 
-    const onClickAddScene = useCallback(
-        () => activeChapter && sceneBroker.add(activeChapter.id),
-        [activeChapter, sceneBroker]
-    )
+    const onClickAddScene = useCallback(() => {
+        if (!activeChapter) {
+            alert('Cannot add scene, missing active chapter')
+            return
+        }
+        InputModal.Show('A new scene title').then((new_title) => {
+            if (new_title.length < 3) {
+                alert('A new scene needs to be atleast 3 characters wrong')
+                return
+            }
+            sceneBroker.create(activeChapter.id, new_title).then(([scene, chapter]) => {
+                setActiveScene(chapter, scene)
+            })
+        })
+    }, [activeChapter, sceneBroker])
 
     if (activeChapter === undefined) {
         return <>No active chapter</>
