f2b5d7d6b53989ed1e13446c7c5e7b5b29ac99da
devdave <devdave@ominian.net>
2023-07-24 12:32:15 -0600

    More progress on toon selection

--- ui/src/modes/edit/continuous_panel/SceneCharacters.tsx
+++ ui/src/modes/edit/continuous_panel/SceneCharacters.tsx
@@ -1,7 +1,8 @@
-import { Text } from '@mantine/core'
-import { Scene } from '@src/types'
+import { Select, Text } from '@mantine/core'
+import { Character, Scene } from '@src/types'
 import { useAppContext } from '@src/App.context'
-import { map } from 'lodash'
+import { map, values } from 'lodash'
+import { useEditorContext } from '@src/modes/edit/Editor.context'
 
 interface SceneCharactersProps {
     scene: Scene
@@ -9,9 +10,22 @@ interface SceneCharactersProps {
 
 export const SceneCharacters: React.FC<SceneCharactersProps> = ({ scene }) => {
     const { activeBook } = useAppContext()
+    const { characterBroker } = useEditorContext()
+
+    const { data: sceneCharacters } = characterBroker.listByScene(scene)
+
+    const toons_list =
+        sceneCharacters === undefined
+            ? []
+            : sceneCharacters.map((toon: Character) => ({ label: toon.name, value: toon.id }))
+
     return (
         <>
-            <Text>Character</Text>
+            <Select
+                data={toons_list}
+                searchable
+                creatable
+            />
             {map(scene.characters, (toon) => (
                 <Text key={toon.id}>{toon.name}</Text>
             ))}
