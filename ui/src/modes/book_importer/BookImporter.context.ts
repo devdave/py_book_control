df73a7f7f02182582cbe51fd2226816b78af9373
devdave <devdave@ominian.net>
2023-08-07 16:54:55 -0600

    Importer has its own minor context

--- ui/src/modes/book_importer/BookImporter.context.ts
+++ ui/src/modes/book_importer/BookImporter.context.ts
@@ -1,5 +1,12 @@
 import { BatchBrokerReturns } from '@src/modes/book_importer/lib/BatchBroker'
+import { createContext, useContext } from 'react'
+import { ActiveStage } from '@src/modes/book_importer/lib/active-stage'
 
 export interface BookImporterContext {
+    activeStage: ActiveStage
     batchBroker: BatchBrokerReturns
 }
+
+export const ImportContext = createContext<BookImporterContext>({} as BookImporterContext)
+
+export const useImportContext = () => useContext(ImportContext)
--- ui/src/modes/book_importer/BookImporter.tsx
+++ ui/src/modes/book_importer/BookImporter.tsx
@@ -1,17 +1,29 @@
-import React, { useState } from 'react'
+import React, { useMemo } from 'react'
 import { AppShell, LoadingOverlay, Text, Title } from '@mantine/core'
 
 import { useQueryClient } from '@tanstack/react-query'
 import { useAppContext } from '@src/App.context'
 import { BatchBroker } from '@src/modes/book_importer/lib/BatchBroker'
 import { ImporterHeader } from '@src/modes/book_importer/ImporterHeader'
-import { BookImporterStepper } from '@src/modes/book_importer/BookImporterStepper'
+import { BookImporterStepper } from '@src/modes/book_importer/steps/BookImporterStepper'
+import { BookImporterContext, ImportContext } from '@src/modes/book_importer/BookImporter.context'
+import { ActiveStage, ActiveStages } from '@src/modes/book_importer/lib/active-stage'
+import { Process } from '@src/modes/book_importer/process/Process'
 
 export const BookImporter = () => {
     const { api } = useAppContext()
     const queryClient = useQueryClient()
 
     const batchBroker = BatchBroker({ api, queryClient })
+    const activeStage = new ActiveStage()
+
+    const bookContextValue = useMemo<BookImporterContext>(
+        () => ({
+            activeStage,
+            batchBroker
+        }),
+        [batchBroker]
+    )
 
     const { data: batch, isLoading: batchLoading, status: batchStatus } = batchBroker.fetch()
 
@@ -35,8 +47,11 @@ export const BookImporter = () => {
     const header = <ImporterHeader />
 
     return (
-        <AppShell header={header}>
-            <BookImporterStepper batch={batch} />
-        </AppShell>
+        <ImportContext.Provider value={bookContextValue}>
+            <AppShell header={header}>
+                {activeStage.current === ActiveStages.INITIAL && <BookImporterStepper batch={batch} />}
+                {activeStage.current === ActiveStages.PROCESS && <Process />}
+            </AppShell>
+        </ImportContext.Provider>
     )
 }
