007dcb9d3c0941984d25d273acf82c106ef9edc4
devdave <devdave@ominian.net>
2023-08-07 04:59:20 -0600

    cleanup

--- ui/src/modes/book_importer/BookImporter.tsx
+++ ui/src/modes/book_importer/BookImporter.tsx
@@ -1,25 +1,17 @@
 import React, { useState } from 'react'
-import { AppShell, Button, Center, Divider, LoadingOverlay, Space, Stepper, Text, Title } from '@mantine/core'
-import { ImporterGreeting } from '@src/modes/book_importer/ImporterGreeting'
-import { InitialSetup } from '@src/modes/book_importer/InitialSetup'
-import { DocumentSelector } from '@src/modes/book_importer/DocumentSelector'
+import { AppShell, LoadingOverlay, Text, Title } from '@mantine/core'
 
 import { useQueryClient } from '@tanstack/react-query'
 import { useAppContext } from '@src/App.context'
-import { VerifyImport } from '@src/modes/book_importer/VerifyImport'
 import { BatchBroker } from '@src/modes/book_importer/lib/BatchBroker'
 import { ImporterHeader } from '@src/modes/book_importer/ImporterHeader'
+import { BookImporterStepper } from '@src/modes/book_importer/BookImporterStepper'
 
 export const BookImporter = () => {
     const { api } = useAppContext()
     const queryClient = useQueryClient()
 
     const batchBroker = BatchBroker({ api, queryClient })
-
-    const [active, setActive] = useState(0)
-
-    const nextStep = () => setActive((current) => (current < 4 ? current + 1 : current))
-    const prevStep = () => setActive((current) => (current > 0 ? current - 1 : current))
 
     const { data: batch, isLoading: batchLoading, status: batchStatus } = batchBroker.fetch()
 
@@ -44,46 +36,7 @@ export const BookImporter = () => {
 
     return (
         <AppShell header={header}>
-            <Stepper
-                active={active}
-                onStepClick={setActive}
-                breakpoint='sm'
-            >
-                <Stepper.Step
-                    label='Greeting'
-                    description='A greeting'
-                >
-                    <ImporterGreeting />
-                </Stepper.Step>
-                <Stepper.Step
-                    label='Book options'
-                    description='Create a blank book to import into.'
-                >
-                    <InitialSetup />
-                </Stepper.Step>
-                <Stepper.Step
-                    label='Document selection'
-                    description='Select the documents to import'
-                >
-                    <DocumentSelector
-                        batch={batch}
-                        nextStep={nextStep}
-                    />
-                </Stepper.Step>
-                <Stepper.Step
-                    label='Verify everything'
-                    description='A quick sanity check'
-                >
-                    <VerifyImport batch={batch} />
-                </Stepper.Step>
-            </Stepper>
-            <Space h='md' />
-            <Divider />
-            <Space h='md' />
-            <Center>
-                <Button onClick={prevStep}>Go back</Button>
-                <Button onClick={nextStep}>Next step</Button>
-            </Center>
+            <BookImporterStepper batch={batch} />
         </AppShell>
     )
 }
