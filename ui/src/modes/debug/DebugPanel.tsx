73cca7c105f26a25b7d30e5a0c1447d909f275f6
devdave <devdave@ominian.net>
2023-08-07 04:37:05 -0600

    make eslint/tslint happy

--- ui/src/modes/debug/DebugPanel.tsx
+++ ui/src/modes/debug/DebugPanel.tsx
@@ -8,13 +8,13 @@ export const DebugPanel = () => {
     const { api, switchBoard } = useAppContext()
 
     const longTask = () => {
-        const responder = (response: string) => {
+        const responder = (response: Record<string, string>) => {
             ShowError('Remote', `Remote says: ${response.msg} in ${response}`)
         }
 
         const callBackId = switchBoard.generate(responder)
 
-        api.debug_long_task(callBackId)
+        api.debug_long_task(callBackId).then()
     }
 
     return (
