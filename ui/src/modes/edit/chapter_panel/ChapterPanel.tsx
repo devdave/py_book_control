91b11f6a6adafc9cde2ccbd645eda21d67f4e5f4
devdave <devdave@ominian.net>
2023-08-09 12:56:44 -0600

    Add in a new re-import/import facts section

--- ui/src/modes/edit/chapter_panel/ChapterPanel.tsx
+++ ui/src/modes/edit/chapter_panel/ChapterPanel.tsx
@@ -1,19 +1,43 @@
-import { Text } from '@mantine/core'
+import { Button, Text } from '@mantine/core'
 import React from 'react'
 import { useEditorContext } from '@src/modes/edit/Editor.context'
 import { type Chapter } from '@src/types'
+import { useAppContext } from '@src/App.context'
 
 interface ChapterPanelProps {
     chapter: Chapter
 }
 
 export const ChapterPanel: React.FC<ChapterPanelProps> = ({ chapter }) => {
+    const { api } = useAppContext()
+    const { chapterBroker } = useEditorContext()
+
     //Let's see what we got
     console.log('ActiveChapter', chapter)
+
+    const onClickReimport = () => {
+        api.importer_reimport_chapter(chapter.id).then(() =>
+            chapterBroker.clearChapterCache(chapter.book_id, chapter.id).then()
+        )
+    }
 
     return (
         <>
             <Text>Chapter Panel</Text>
+            <dl>
+                <dt>Source file</dt>
+                <dd>{chapter.source_file}</dd>
+
+                <dt>Source size</dt>
+                <dd>{chapter.source_size}</dd>
+
+                <dt>Last modified</dt>
+                <dd>TO FIX! {chapter.last_imported}</dd>
+
+                <dt>Last imported</dt>
+                <dt>{chapter.last_imported}</dt>
+            </dl>
+            {chapter.source_file && <Button onClick={onClickReimport}>Re-import</Button>}
         </>
     )
 }
