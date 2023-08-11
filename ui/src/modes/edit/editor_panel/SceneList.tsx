2c45d7b2b5ca780b3b40b61bf2a71f06e442aa33
devdave <devdave@ominian.net>
2023-08-11 10:49:34 -0600

    similar, disable scroll to and autofocus

--- ui/src/modes/edit/editor_panel/SceneList.tsx
+++ ui/src/modes/edit/editor_panel/SceneList.tsx
@@ -25,20 +25,20 @@ const SceneList = () => {
     useEffect(() => {
         if (activeScene && accordionRefs.current[activeScene.id]) {
             const currentElement = accordionRefs.current[activeScene.id]
-            currentElement.scrollIntoView({
-                behavior: 'smooth',
-                block: 'end',
-                inline: 'nearest'
-            })
+            // currentElement.scrollIntoView({
+            //     behavior: 'smooth',
+            //     block: 'end',
+            //     inline: 'nearest'
+            // })
             const ta = currentElement.querySelector('.mainContent textarea') as HTMLTextAreaElement
-            if (ta) {
-                ta.setSelectionRange(ta.value.length, ta.value.length)
-                ta.scrollTop = ta.scrollHeight
-                ta.blur()
-                ta.focus()
-                ta.selectionStart = ta.value.length || 0
-                ta.setSelectionRange(ta.value.length, ta.value.length + 1)
-                ta.value += ''
+            if (ta && ta !== document.activeElement) {
+                // ta.setSelectionRange(ta.value.length, ta.value.length)
+                // ta.scrollTop = ta.scrollHeight
+                // ta.blur()
+                // ta.focus()
+                // ta.selectionStart = ta.value.length || 0
+                // ta.setSelectionRange(ta.value.length, ta.value.length + 1)
+                // ta.value += ''
             }
         }
     }, [activeScene, activeScene?.id])
