c5d12f4e67d265d3d5409de34554919aa0b92694
devdave <devdave@ominian.net>
2023-11-28 09:57:41 -0700

    screwed this one up

--- ui2/src/lib/switchboard.ts
+++ ui2/src/lib/switchboard.ts
@@ -1,63 +1,64 @@
-import { Deferred } from './deferred'
+import { Deferred } from "./deferred";
 
-type deferredCallback = (...args: never) => void
-type deferredCallbacks = { [key: string]: deferredCallback }
+type deferredCallback = (...args: [unknown]) => void;
+type deferredCallbacks = { [key: string]: deferredCallback };
 
 declare global {
-    interface Window {
-        returnCall: (identifier: string, result: never) => void
-        callBack: (identifier: string, result: never) => void
-    }
+  interface Window {
+    returnCall: (identifier: string, result: never) => void;
+    callBack: (identifier: string, result: never) => void;
+  }
 }
 
 export class Switchboard {
-    callbacks: deferredCallbacks
-    constructor() {
-        this.callbacks = {}
-        window.returnCall = this.returnVal.bind(this)
-        window.callBack = this.callBack.bind(this)
-    }
+  callbacks: deferredCallbacks;
+  constructor() {
+    this.callbacks = {};
+    window.returnCall = this.returnVal.bind(this);
+    window.callBack = this.callBack.bind(this);
+  }
 
-    public generate(callBack: (response: never) => void): string {
-        const id = this.generateId()
-        this.callbacks[id] = callBack
-        return id
-    }
+  public generate(callBack: (response: unknown) => void): string {
+    const id = this.generateId();
+    this.callbacks[id] = callBack;
+    return id;
+  }
 
-    private generateId() {
-        let newId = ''
-        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYabcdefghijklmnopqrstuvwxyz0123456789'
-        const charLen = characters.length
+  private generateId() {
+    let newId = "";
+    const characters =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYabcdefghijklmnopqrstuvwxyz0123456789";
+    const charLen = characters.length;
 
-        while (newId.length < 12) {
-            newId += characters.charAt(Math.floor(Math.random() * charLen))
-        }
-        return newId
+    while (newId.length < 12) {
+      newId += characters.charAt(Math.floor(Math.random() * charLen));
     }
+    return newId;
+  }
 
-    public register(d: Deferred) {
-        const identifer = this.generateId()
-        this.callbacks[identifer] = d.callback
-        return identifer
-    }
+  public register(d: Deferred) {
+    const identifer = this.generateId();
+    this.callbacks[identifer] = d.callback;
+    return identifer;
+  }
 
-    public deregister(identifier: string) {
-        if (this.callbacks[identifier]) {
-            delete this.callbacks[identifier]
-        }
+  public deregister(identifier: string) {
+    if (this.callbacks[identifier]) {
+      delete this.callbacks[identifier];
     }
+  }
 
-    public callBack(identifier: string, result: never) {
-        console.debug('Called ', identifier, result)
-        if (this.callbacks[identifier]) {
-            this.callbacks[identifier](result)
-        }
+  public callBack(identifier: string, result: unknown) {
+    console.debug("Called ", identifier, result);
+    if (this.callbacks[identifier]) {
+      this.callbacks[identifier](result);
     }
+  }
 
-    public returnVal(identifier: string, result: never) {
-        if (this.callbacks[identifier]) {
-            this.callbacks[identifier](result)
-            delete this.callbacks[identifier]
-        }
+  public returnVal(identifier: string, result: unknown) {
+    if (this.callbacks[identifier]) {
+      this.callbacks[identifier](result);
+      delete this.callbacks[identifier];
     }
+  }
 }
