181df6cb3d3eeadcf0d5656d0c634c29228a97c9
devdave <devdave@ominian.net>
2023-11-29 05:15:03 -0700

    Add to emulate # anchor support

---
+++ ui2/src/lib/ScrollToAnchor.tsx
@@ -0,0 +1,34 @@
+import { useEffect, useRef } from 'react';
+import { useLocation } from 'react-router-dom';
+//Sourced from https://dev.to/mindactuate/scroll-to-anchor-element-with-react-router-v6-38op
+function ScrollToAnchor() {
+    const yOffset = -290
+    const location = useLocation();
+    const lastHash = useRef('');
+
+    // listen to location change using useEffect with location as dependency
+    // https://jasonwatmore.com/react-router-v6-listen-to-location-route-change-without-history-listen
+    useEffect(() => {
+        if (location.hash) {
+            lastHash.current = location.hash.slice(1); // safe hash for further use after navigation
+        }
+
+        if (lastHash.current && document.getElementById(lastHash.current)) {
+            setTimeout(() => {
+
+                const element = document.getElementById(lastHash.current)
+                if(element){
+                    const y = element.getBoundingClientRect().top + window.scrollY + yOffset;
+                    // @ts-expect-error top is not recognized but it is correct
+                    element.scrollIntoView({ top: y, behavior: 'smooth'});
+                    lastHash.current = '';
+                }
+
+            }, 100);
+        }
+    }, [location]);
+
+    return null;
+}
+
+export default ScrollToAnchor;
