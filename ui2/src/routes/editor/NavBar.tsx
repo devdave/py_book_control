124a9818449cfecd47c7396db8604210c0ed8778
devdave <devdave@ominian.net>
2023-11-29 12:32:25 -0700

    Work on improving nav bar

--- ui2/src/routes/editor/NavBar.tsx
+++ ui2/src/routes/editor/NavBar.tsx
@@ -1,7 +1,9 @@
 import {NavLink as RoutedLink, useParams} from "react-router-dom";
-import {Button, NavLink, Title} from "@mantine/core";
+import {Box, Button, Center, Group, NavLink, SegmentedControl, Title} from "@mantine/core";
 import {Book, Chapter, Scene} from "@src/types.ts";
 import {AppShell} from "@mantine/core";
+import {IconArticle, IconBook, IconList, IconPlus} from "@tabler/icons-react";
+import {useState} from "react";
 
 
 interface NavBarParams {
@@ -10,15 +12,48 @@ interface NavBarParams {
 
 export const NavBar:React.FC<NavBarParams> = ({book}) => {
 
-    const params = useParams<{book_id:string, chapter_id?:string}>()
+    const [editMode, setEditMode] = useState("Detailed")
+
+    const {chapter_id} = useParams<{chapter_id?:string}>()
+
+
+
 
 
     return (
         <AppShell.Navbar>
-            <Button>Create new chapter</Button>
-            <NavLink label={<Title>{book.title}</Title>} component={RoutedLink} to={`/book/${book.id}`}/>
-            { book.chapters.map((chapter:Chapter)=>
-                <NavLink label={`Chapter: ${chapter.title}`} component={RoutedLink} to={`/book/${book.id}/chapter/${chapter.id}`}>
+            <Group>
+                <SegmentedControl
+                    disabled={!chapter_id}
+                    value={editMode}
+                    onChange={setEditMode}
+                    data={[
+                        {
+                            value: "Detailed",
+                            label: (
+                                <Center>
+                                    <IconList />
+                                    <Box ml={10}>Detailed</Box>
+                                </Center>
+                            )
+                        },
+                        {
+                            value: "Flow",
+                            label: (
+                                <Center>
+                                    <IconArticle />
+                                    <Box ml={10}>Flow</Box>
+                                </Center>
+                            )
+                        }
+                    ]}
+                />
+            </Group>
+            <Button><IconPlus />Create new chapter</Button>
+            <NavLink label={<><IconBook/><Title>Book: {book.title}</Title></>} component={RoutedLink} to={`/book/${book.id}`}/>
+
+            { book.chapters.map((chapter:Chapter, idx)=>
+                <NavLink label={`${idx+1}. Chapter: ${chapter.title}`} component={RoutedLink} to={`/book/${book.id}/chapter/${chapter.id}`}>
 
                     {chapter.scenes.map((scene:Scene)=>
                         <NavLink label={scene.title} component={RoutedLink} to={`/book/${book.id}/chapter/${chapter.id}#${scene.id}`}>
