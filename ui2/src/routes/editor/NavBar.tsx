010db40a5fec6c689276e2cc9d22fcb3dde2fc6b
devdave <devdave@ominian.net>
2023-11-29 06:25:07 -0700

    starting refactor

--- ui2/src/routes/editor/NavBar.tsx
+++ ui2/src/routes/editor/NavBar.tsx
@@ -1,5 +1,5 @@
 import {NavLink as RoutedLink} from "react-router-dom";
-import {NavLink, Title} from "@mantine/core";
+import {Button, NavLink, Title} from "@mantine/core";
 import {Book, Chapter, Scene} from "@src/types.ts";
 import {AppShell} from "@mantine/core";
 
@@ -14,6 +14,7 @@ export const NavBar:React.FC<NavBarParams> = ({book}) => {
 
     return (
         <AppShell.Navbar>
+            <Button>Create new chapter</Button>
             <NavLink label={<Title>{book.title}</Title>} component={RoutedLink} to={`/book/${book.id}`}/>
             { book.chapters.map((chapter:Chapter)=>
                 <NavLink label={`Chapter: ${chapter.title}`} component={RoutedLink} to={`/book/${book.id}/chapter/${chapter.id}`}>
