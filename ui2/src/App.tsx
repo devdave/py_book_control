b6417212f0931dd99f5718c33f169f0d9295d5f2
devdave <devdave@ominian.net>
2023-11-29 05:16:19 -0700

    spacing fix + add chapter broker

--- ui2/src/App.tsx
+++ ui2/src/App.tsx
@@ -14,185 +14,189 @@ import { ThemeProvider } from "@src/ThemeProvider.tsx";
 import { WebRoot } from "@src/routes/WebRoot.tsx";
 import { LoadingOverlay } from "@mantine/core";
 import { forEach } from "lodash";
+import {ChapterBroker} from "@src/brokers/ChapterBroker.ts";
 
 let appIsReady = false;
 
 export const App = () => {
-  const queryClient = useQueryClient();
+    const queryClient = useQueryClient();
 
-  const [fonts, setFonts] = useState<Set<string>>(new Set());
+    const [fonts, setFonts] = useState<Set<string>>(new Set());
 
-  const [defaultsDone, setDefaultsDone] = useState(false);
+    const [defaultsDone, setDefaultsDone] = useState(false);
 
-  const boundary = useMemo(() => new Boundary(), []);
+    const boundary = useMemo(() => new Boundary(), []);
 
-  const api = useMemo(() => new APIBridge(boundary), [boundary]);
+    const api = useMemo(() => new APIBridge(boundary), [boundary]);
 
-  const switchBoard = useMemo(() => new Switchboard(), []);
+    const switchBoard = useMemo(() => new Switchboard(), []);
 
-  /**
+    /**
    * Brokers
    */
-  const bookBroker = BookBroker({ api, queryClient });
+    const bookBroker = BookBroker({ api, queryClient });
 
-  const sceneStatusBroker = SceneStatusBroker({ api, queryClient });
+    const sceneStatusBroker = SceneStatusBroker({ api, queryClient });
 
-  const chatBroker = ChatBroker("http://127.0.0.1:8000/");
+    const chatBroker = ChatBroker("http://127.0.0.1:8000/");
 
-  /**
+    const chapterBroker = ChapterBroker({api, queryClient})
+
+    /**
    * Application settings
    *
    */
-  const settingsSetter = useMutation({
-    mutationFn: ({ name, value }) => api.setSetting(name, value),
-    onSuccess: (
-      _data: undefined,
-      { name, value }: { name: string; value: string },
-    ) => {
-      console.log(`Updated setting.${name} with ${value}`);
-      queryClient.setQueryData(["setting", name], () => value);
+    const settingsSetter = useMutation({
+        mutationFn: ({ name, value }) => api.setSetting(name, value),
+        onSuccess: (
+            _data: undefined,
+            { name, value }: { name: string; value: string },
+        ) => {
+            console.log(`Updated setting.${name} with ${value}`);
+            queryClient.setQueryData(["setting", name], () => value);
 
-      queryClient
-        .invalidateQueries({
-          queryKey: ["settings"],
-          exact: true,
-          refetchType: "active",
-        })
-        .then();
-    },
-  });
-
-  const fetchSetting = (name: string) =>
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    useQuery({
-      queryKey: ["setting", name],
-      queryFn: () => api.getSetting(name),
+            queryClient
+                .invalidateQueries({
+                    queryKey: ["settings"],
+                    exact: true,
+                    refetchType: "active",
+                })
+                .then();
+        },
     });
 
-  const reconcileSettings = useCallback(
-    (values: ApplicationSetting[]) => {
-      console.log(`Got bulk settings ${JSON.stringify(values)}`);
-      forEach(values, (setting) => {
-        queryClient.setQueryData(
-          ["setting", setting.name],
-          () => setting.value,
-        );
-      });
-    },
-    [queryClient],
-  );
+    const fetchSetting = (name: string) =>
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+        useQuery({
+            queryKey: ["setting", name],
+            queryFn: () => api.getSetting(name),
+        });
 
-  const bulkDefaultSetter = (changeset: object[]): Promise<object> =>
-    api.bulkDefaultSettings(changeset);
+    const reconcileSettings = useCallback(
+        (values: ApplicationSetting[]) => {
+            console.log(`Got bulk settings ${JSON.stringify(values)}`);
+            forEach(values, (setting) => {
+                queryClient.setQueryData(
+                    ["setting", setting.name],
+                    () => setting.value,
+                );
+            });
+        },
+        [queryClient],
+    );
 
-  const bulkFetchSettings = (): Promise<ApplicationSetting[]> =>
-    api.fetchAllSettings();
+    const bulkDefaultSetter = (changeset: object[]): Promise<object> =>
+        api.bulkDefaultSettings(changeset);
 
-  const settings = useSettings<AppSettingValues>({
-    bulkFetchSettings,
-    bulkDefaultSetter,
-    getter: fetchSetting,
-    setter: settingsSetter,
-    defaultSettings: {
-      fontName: "Calibri",
-      lineHeight: 150,
-      fontSize: 18,
-      fontWeight: 400,
-      debounceTime: 800,
-      dontAskOnSplit: false,
-      dontAskOnClear2Delete: false,
-      defaultSceneStatus: "-1",
-      lastImportedPath: "",
-    },
-  });
+    const bulkFetchSettings = (): Promise<ApplicationSetting[]> =>
+        api.fetchAllSettings();
 
-  /**
+    const settings = useSettings<AppSettingValues>({
+        bulkFetchSettings,
+        bulkDefaultSetter,
+        getter: fetchSetting,
+        setter: settingsSetter,
+        defaultSettings: {
+            fontName: "Calibri",
+            lineHeight: 150,
+            fontSize: 18,
+            fontWeight: 400,
+            debounceTime: 800,
+            dontAskOnSplit: false,
+            dontAskOnClear2Delete: false,
+            defaultSceneStatus: "-1",
+            lastImportedPath: "",
+        },
+    });
+
+    /**
    * FONT STUFF
    */
-  const checkFonts = useCallback(() => {
-    const fontAvailable = new Set<string>();
+    const checkFonts = useCallback(() => {
+        const fontAvailable = new Set<string>();
 
-    // eslint-disable-next-line no-restricted-syntax
-    for (const font of font_set.values()) {
-      if (document.fonts.check(`12px "${font}"`)) {
-        fontAvailable.add(font);
-      }
-    }
+        // eslint-disable-next-line no-restricted-syntax
+        for (const font of font_set.values()) {
+            if (document.fonts.check(`12px "${font}"`)) {
+                fontAvailable.add(font);
+            }
+        }
 
-    setFonts(fontAvailable);
-  }, []);
+        setFonts(fontAvailable);
+    }, []);
 
-  useEffect(() => {
-    document.fonts.ready.then(() => {
-      checkFonts();
-    });
-  }, [checkFonts]);
+    useEffect(() => {
+        document.fonts.ready.then(() => {
+            checkFonts();
+        });
+    }, [checkFonts]);
 
-  /**
+    /**
    * Finalization
    *
    */
-  const appContextValue = useMemo<AppContextValue>(
-    () => ({
-      api,
-      settings,
-      switchBoard,
-      fonts,
-      bookBroker,
-      sceneStatusBroker,
-      chatBroker,
-    }),
-    [
-      api,
-      settings,
-      switchBoard,
-      fonts,
-      bookBroker,
-      sceneStatusBroker,
-      chatBroker,
-    ],
-  );
-
-  console.log("Appcontext: ", appContextValue);
-
-  /**
-   * Finalization
-   *
-   */
-
-  const doReady = useCallback(async () => {
-    if (!defaultsDone) {
-      settings.reconcile(reconcileSettings);
-      setDefaultsDone(true);
-    }
-    appIsReady = true;
-  }, [defaultsDone, settings, reconcileSettings, api]);
-
-  useEffect(() => {
-    if (window.pywebview !== undefined && window.pywebview.api !== undefined) {
-      doReady().then();
-    } else {
-      window.addEventListener(PYWEBVIEWREADY, doReady, { once: true });
-    }
-  }, [doReady]);
-
-  if (!appIsReady) {
-    return (
-      <AppContext.Provider value={appContextValue}>
-        <ThemeProvider>
-          <LoadingOverlay />
-        </ThemeProvider>
-      </AppContext.Provider>
+    const appContextValue = useMemo<AppContextValue>(
+        () => ({
+            api,
+            settings,
+            switchBoard,
+            fonts,
+            bookBroker,
+            chapterBroker,
+            sceneStatusBroker,
+            chatBroker,
+        }),
+        [
+            api,
+            settings,
+            switchBoard,
+            fonts,
+            bookBroker,
+            sceneStatusBroker,
+            chatBroker,
+        ],
     );
-  }
 
-  return (
-    <>
-      <AppContext.Provider value={appContextValue}>
-        <ThemeProvider>
-          <WebRoot />
-        </ThemeProvider>
-      </AppContext.Provider>
-    </>
-  );
+    console.log("Appcontext: ", appContextValue);
+
+    /**
+   * Finalization
+   *
+   */
+
+    const doReady = useCallback(async () => {
+        if (!defaultsDone) {
+            settings.reconcile(reconcileSettings);
+            setDefaultsDone(true);
+        }
+        appIsReady = true;
+    }, [defaultsDone, settings, reconcileSettings, api]);
+
+    useEffect(() => {
+        if (window.pywebview !== undefined && window.pywebview.api !== undefined) {
+            doReady().then();
+        } else {
+            window.addEventListener(PYWEBVIEWREADY, doReady, { once: true });
+        }
+    }, [doReady]);
+
+    if (!appIsReady) {
+        return (
+            <AppContext.Provider value={appContextValue}>
+                <ThemeProvider>
+                    <LoadingOverlay />
+                </ThemeProvider>
+            </AppContext.Provider>
+        );
+    }
+
+    return (
+        <>
+            <AppContext.Provider value={appContextValue}>
+                <ThemeProvider>
+                    <WebRoot />
+                </ThemeProvider>
+            </AppContext.Provider>
+        </>
+    );
 };
