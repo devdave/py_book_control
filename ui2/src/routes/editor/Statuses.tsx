a6a80e2da4db0b151b2b12a9fd082c9bc592d5c3
devdave <devdave@ominian.net>
2023-11-29 20:04:12 -0700

    new statuses view

---
+++ ui2/src/routes/editor/Statuses.tsx
@@ -0,0 +1,121 @@
+import {Book} from "@src/types.ts";
+import {useAppContext} from "@src/App.context.ts";
+import {ActionIcon, Button, LoadingOverlay, Radio} from "@mantine/core";
+import {createSceneStatus, editSceneStatus} from "@src/common/SceneStatusModals.tsx";
+import { ShowError } from '@src/widget/ShowErrorNotification'
+import {IconEdit, IconFlagFilled, IconX} from "@tabler/icons-react";
+
+export const Statuses = ({book}:{book:Book}) => {
+
+    const {sceneStatusBroker, settings } = useAppContext()
+
+    const [defaultSceneStatus, , setDefaultSceneStatus] = settings.makeState('defaultSceneStatus')
+
+    const {data, isLoading, isError} = sceneStatusBroker.fetchAll(book.id, true)
+
+    if(isLoading){
+        return <LoadingOverlay visible/>
+    }
+
+    if(isError){
+        return (
+            <>
+                <p>There was a problem loading statuses</p>
+            </>
+        )
+    }
+
+    return (
+        <>
+            <h2>Statuses</h2>
+
+            <Button
+                onClick={() => {
+                    createSceneStatus().then((status) => {
+                        if (!status.name || status.name.length < 3) {
+                            ShowError('Error', 'Status name needs to be aleast 3 characters long')
+                        } else if (status && status.name && status.color) {
+                            sceneStatusBroker
+                                .create(book.id, status.name, status.color)
+                                .catch((reason) => {
+                                    if (reason instanceof Error || reason.message !== undefined) {
+                                        ShowError('Failed to create', reason.message)
+                                    }
+                                })
+                        }
+                    })
+                }}
+            >
+                Create new status
+            </Button>
+
+            <table>
+                <tr>
+                    <th align='left'>Default status</th>
+                </tr>
+                <tbody>
+                    <Radio.Group
+                        name='defaultSceneStatus'
+                        value={defaultSceneStatus}
+                        onChange={(value) => {
+                            setDefaultSceneStatus(value)
+                        }}
+                    >
+                        <tr>
+                            <td>
+                                <Radio
+                                    value='-1'
+                                    label='Nothing'
+                                />
+                            </td>
+                        </tr>
+                        {data?.map((sstatus) => (
+                            <tr key={sstatus.id}>
+                                <td>
+                                    <Radio
+                                        value={sstatus.id}
+                                        label={sstatus.name}
+                                    />
+                                </td>
+                                <td>
+                                    <IconFlagFilled style={{ color: sstatus.color }} />
+                                </td>
+                                <td>
+                                    <ActionIcon
+                                        onClick={() => {
+                                            editSceneStatus(sstatus.id, sceneStatusBroker.get).then(
+                                                (status) => {
+                                                    console.log('Edit gave ', status)
+                                                    if (status && status.id !== undefined) {
+                                                        sceneStatusBroker.update(
+                                                            book.id,
+                                                            status.id,
+                                                            status
+                                                        )
+                                                    }
+                                                }
+                                            )
+                                        }}
+                                    >
+                                        <IconEdit />
+                                    </ActionIcon>
+                                </td>
+
+                                <ActionIcon
+                                    onClick={() => {
+                                        sceneStatusBroker.delete(book.id, sstatus.id)
+                                    }}
+                                >
+                                    <IconX />
+                                </ActionIcon>
+                                <td />
+                            </tr>
+                        ))}
+                    </Radio.Group>
+                </tbody>
+            </table>
+
+        </>
+    )
+
+}
