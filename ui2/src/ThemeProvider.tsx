ac45b32201cda81d55c3d270223190c0c4718c2b
devdave <devdave@ominian.net>
2023-11-30 09:08:41 -0700

    bring in notification system styles and provider

--- ui2/src/ThemeProvider.tsx
+++ ui2/src/ThemeProvider.tsx
@@ -1,74 +1,79 @@
 import {
-  localStorageColorSchemeManager,
-  createTheme,
-  MantineProvider,
-  Textarea,
+    localStorageColorSchemeManager,
+    createTheme,
+    MantineProvider, 
+    Textarea,
 } from "@mantine/core";
 
 import { ReactNode, useEffect } from "react";
 import { ModalsProvider } from "@mantine/modals";
 import { useAppContext } from "@src/App.context";
+import {Notifications} from "@mantine/notifications";
 
 interface ThemeProviderProps {
   children: ReactNode;
 }
 
 export function ThemeProvider({ children }: ThemeProviderProps) {
-  const { settings } = useAppContext();
+    const { settings } = useAppContext();
 
-  console.log("Settings: ", settings);
+    console.log("Settings: ", settings);
 
-  const colorSchemeManager = localStorageColorSchemeManager({
-    key: "bookapp_color_scheme",
-  });
+    const colorSchemeManager = localStorageColorSchemeManager({
+        key: "bookapp_color_scheme",
+    });
 
-  const [fontName] = settings.makeState("fontName");
-  const [fontSize] = settings.makeState("fontSize");
-  const [fontWeight] = settings.makeState("fontWeight");
-  const [lineHeight] = settings.makeState("lineHeight");
+    const [fontName] = settings.makeState("fontName");
+    const [fontSize] = settings.makeState("fontSize");
+    const [fontWeight] = settings.makeState("fontWeight");
+    const [lineHeight] = settings.makeState("lineHeight");
 
-  const safeFont = fontName || "Calibre";
+    const safeFont = fontName || "Calibre";
 
-  const theme = createTheme({
-    components: {
-      Textarea: Textarea.extend({
-        styles: {
-          root: {
-            fontFamily: `"${safeFont}"`,
-            fontSize: `${fontSize}px`,
-            fontWeight,
-            lineHeight: `${lineHeight}%`,
-            textAlign: "justify",
-            whiteSpace: "pre-wrap",
-          },
-          input: {
-            fontFamily: `"${safeFont}"`,
-            fontSize: `${fontSize}px`,
-            fontWeight,
-            lineHeight: `${lineHeight}%`,
-            textAlign: "justify",
-            whiteSpace: "pre-wrap",
-          },
+    const theme = createTheme({
+        components: {
+            Textarea: Textarea.extend({
+                styles: {
+                    root: {
+                        fontFamily: `"${safeFont}"`,
+                        fontSize: `${fontSize}px`,
+                        fontWeight,
+                        lineHeight: `${lineHeight}%`,
+                        textAlign: "justify",
+                        whiteSpace: "pre-wrap",
+                    },
+                    input: {
+                        fontFamily: `"${safeFont}"`,
+                        fontSize: `${fontSize}px`,
+                        fontWeight,
+                        lineHeight: `${lineHeight}%`,
+                        textAlign: "justify",
+                        whiteSpace: "pre-wrap",
+                    },
+                },
+            }),
         },
-      }),
-    },
-  });
+    });
 
-  useEffect(() => {
-    const rootStyle = document.documentElement.style;
-    rootStyle.setProperty("--pbc-fontFamily", fontName || null);
-    rootStyle.setProperty("--pbc-fontSize", fontSize?.toString() || null);
-    rootStyle.setProperty("--pbc-fontWeight", fontWeight?.toString() || null);
-    rootStyle.setProperty("--pbc-lineHeight", `${lineHeight?.toString()}%`);
-  }, [fontName, fontSize, fontWeight, lineHeight]);
+    useEffect(() => {
+        const rootStyle = document.documentElement.style;
+        rootStyle.setProperty("--pbc-fontFamily", fontName || null);
+        rootStyle.setProperty("--pbc-fontSize", fontSize?.toString() || null);
+        rootStyle.setProperty("--pbc-fontWeight", fontWeight?.toString() || null);
+        rootStyle.setProperty("--pbc-lineHeight", `${lineHeight?.toString()}%`);
+    }, [fontName, fontSize, fontWeight, lineHeight]);
 
-  return (
-    <MantineProvider
-      defaultColorScheme="dark"
-      colorSchemeManager={colorSchemeManager}
-      theme={theme}
-    >
-      <ModalsProvider>{children}</ModalsProvider>
-    </MantineProvider>
-  );
+    return (
+        <MantineProvider
+            defaultColorScheme="dark"
+            colorSchemeManager={colorSchemeManager}
+            theme={theme}
+        >
+            <ModalsProvider>
+                <Notifications/>
+                {children}
+            </ModalsProvider>
+
+        </MantineProvider>
+    );
 }
--- ui2/src/main.tsx
+++ ui2/src/main.tsx
@@ -3,23 +3,25 @@ import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import "@mantine/core/styles.css";
+import '@mantine/notifications/styles.css';
+
 import "./global.css";
 import { App } from "@src/App.tsx";
 
 const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: Infinity,
+    defaultOptions: {
+        queries: {
+            staleTime: Infinity,
+        },
     },
-  },
 });
 
 const Main = () => (
-  <QueryClientProvider client={queryClient}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    </QueryClientProvider>
 );
 
 ReactDOM.createRoot(document.getElementById("root")!).render(Main());
