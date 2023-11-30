e20a69b0149c30204e0a2b135921d953ab417396
devdave <devdave@ominian.net>
2023-11-29 20:03:02 -0700

    clean up and use provided book argument

--- ui2/src/brokers/CharacterBroker.ts
+++ ui2/src/brokers/CharacterBroker.ts
@@ -1,37 +1,31 @@
 import APIBridge from '@src/lib/remote'
 import { useCallback } from 'react'
 import { QueryClient, useMutation, useQuery, UseQueryResult } from '@tanstack/react-query'
-import { Book, Chapter, ChapterIndex, Character, Scene } from '@src/types'
+import { Book, Character, Scene } from '@src/types'
 
 export interface CharacterBrokerProps {
     api: APIBridge
     queryClient: QueryClient
-    activeBook: Book
-    activeChapter: Chapter | ChapterIndex | undefined
-    setActiveScene: (chapter: Chapter, scene: Scene) => void
 }
 
 export interface CharacterBrokerFunctions {
-    createAndAdd2Scene: (scene: Scene, new_name: string) => undefined
+    createAndAdd2Scene: (book:Book, scene: Scene, new_name: string) => undefined
     get: (
         book_id: Book['id'],
         character_id: Character['id'],
         enabled: boolean
     ) => UseQueryResult<Character, Error>
-    list: (bookId: Book['id']) => UseQueryResult<Character[], Error>
-    listByScene: (scene: Scene) => UseQueryResult<Character[], Error>
-    update: (changeset: Character) => void
-    delete: (character_id: Character['id']) => void
-    assign2Scene: (scene: Scene, char_id: Character['id']) => string
+    list: (book: Book) => UseQueryResult<Character[], Error>
+    listByScene: (book:Book, scene: Scene) => UseQueryResult<Character[], Error>
+    update: (book: Book, changeset: Character) => void
+    delete: (book:Book, character_id: Character['id']) => void
+    assign2Scene: (book:Book, scene: Scene, char_id: Character['id']) => string
     removeFromScene: (characterId: Character['id'], sceneId: Scene['id']) => Promise<boolean>
 }
 
 export const CharacterBroker = ({
     api,
     queryClient,
-    activeBook,
-    activeChapter,
-    setActiveScene
 }: CharacterBrokerProps): CharacterBrokerFunctions => {
     const fetchCharacter = useCallback(
         (
@@ -46,7 +40,7 @@ export const CharacterBroker = ({
                     queryKey: toon_key,
                     queryFn: () => api.fetch_character(book_id, character_id),
                     enabled
-            })
+                })
         },
         [api]
     )
@@ -62,29 +56,29 @@ export const CharacterBroker = ({
         []
     )
 
-    const _updateCharacter = useMutation<Character, Error, Character>({
-        mutationFn: (character: Character) => api.update_character(character),
-        onSuccess: (updated_character) => {
+    const _updateCharacter = useMutation<Character, Error, {book: Book, character: Character}>({
+        mutationFn: ({book, character}) => api.update_character(book.id, character),
+        onSuccess: (updated_character, {book}) => {
             queryClient.setQueryData(
-                ['book', activeBook.id, 'character', updated_character.id],
+                ['book', book.id, 'character', updated_character.id],
                 () => updated_character
             )
 
             queryClient
                 .invalidateQueries({
-                    queryKey: ['book', activeBook.id, 'character', updated_character.id],
+                    queryKey: ['book', book.id, 'character', updated_character.id],
                     exact: true,
                     refetchType: 'active'
                 })
                 .then()
 
-            queryClient.setQueryData(['book', activeBook.id, 'characters'], (original) =>
+            queryClient.setQueryData(['book', book.id, 'characters'], (original) =>
                 _updateCharacterQueryCacheData(original as Character[], updated_character)
             )
 
             queryClient
                 .invalidateQueries({
-                    queryKey: ['book', activeBook.id, 'characters'],
+                    queryKey: ['book', book.id, 'characters'],
                     exact: true,
                     refetchType: 'active'
                 })
@@ -96,43 +90,41 @@ export const CharacterBroker = ({
     })
 
     const updateCharacter = useCallback(
-        (changeset: Character) => {
-            _updateCharacter.mutate(changeset)
+        (book: Book, character: Character) => {
+            _updateCharacter.mutate({book, character})
         },
         [_updateCharacter]
     )
 
     const deleteCharacter = useCallback(
-        (character_id: string) => {
+        (book: Book, character_id: string) => {
             api.delete_character(character_id).then()
 
             queryClient
                 .invalidateQueries({
-                    queryKey: ['book', activeBook.id, 'characters'],
+                    queryKey: ['book', book.id, 'characters'],
                     exact: true,
                     refetchType: 'active'
                 })
                 .then()
             queryClient
                 .invalidateQueries({
-                    queryKey: ['book', activeBook.id, 'character', character_id],
+                    queryKey: ['book', book.id, 'character', character_id],
                     exact: true,
                     refetchType: 'active'
                 })
                 .then()
         },
-        [api, queryClient, activeBook.id]
+        [api, queryClient]
     )
 
     const assignCharacter2Scene = useCallback(
-        (scene: Scene, char_id: Character['id']) => {
-            api.add_character_to_scene(scene.id, char_id).then((new_scene) =>
-                setActiveScene(activeChapter as Chapter, new_scene as Scene)
-            )
+        (book: Book, scene: Scene, char_id: Character['id']) => {
+            api.add_character_to_scene(scene.id, char_id).then()
 
             queryClient
                 .invalidateQueries({
-                    queryKey: ['book', activeBook.id, 'chapter', scene.chapterId, 'scene', scene.id],
+                    queryKey: ['book', book.id, 'chapter', scene.chapterId, 'scene', scene.id],
                     exact: true,
                     refetchType: 'active'
                 })
@@ -140,7 +132,7 @@ export const CharacterBroker = ({
 
             queryClient
                 .invalidateQueries({
-                    queryKey: ['book', activeBook.id, 'scene', scene.id, 'characters'],
+                    queryKey: ['book', book.id, 'scene', scene.id, 'characters'],
                     exact: true,
                     refetchType: 'active'
                 })
@@ -148,20 +140,20 @@ export const CharacterBroker = ({
 
             queryClient
                 .invalidateQueries({
-                    queryKey: ['book', activeBook.id, 'characters'],
+                    queryKey: ['book', book.id, 'characters'],
                     exact: true,
                     refetchType: 'active'
                 })
                 .then()
             return char_id
         },
-        [activeBook.id, activeChapter, api, queryClient, setActiveScene]
+        [api, queryClient]
     )
 
     const createNewCharacterAndAdd2Scene = useCallback(
-        (scene: Scene, new_name: string) => {
+        (activeBook:Book, scene: Scene, new_name: string) => {
             api.create_new_character_to_scene(activeBook.id, scene.id, new_name).then((new_scene) => {
-                setActiveScene(activeChapter as Chapter, new_scene)
+
                 queryClient.setQueryData(
                     ['book', activeBook.id, 'chapter', scene.chapterId, 'scene', scene.id],
                     new_scene
@@ -182,25 +174,25 @@ export const CharacterBroker = ({
             })
             return undefined
         },
-        [activeBook.id, activeChapter, api, queryClient, setActiveScene]
+        [api, queryClient]
     )
 
     const listCharactersByScene = useCallback(
-        (scene: Scene): UseQueryResult<Character[], Error> =>
+        (activeBook: Book, scene: Scene): UseQueryResult<Character[], Error> =>
             // eslint-disable-next-line react-hooks/rules-of-hooks
             useQuery({
                 queryKey: ['book', activeBook.id, 'scene', scene.id, 'characters'],
                 queryFn: () => api.list_characters_by_scene(scene.id)
             }),
-        [activeBook.id, api]
+        [api]
     )
 
     const listAllCharacters = useCallback(
-        (bookId: Book['id']): UseQueryResult<Character[], Error> =>
+        (book: Book): UseQueryResult<Character[], Error> =>
             // eslint-disable-next-line react-hooks/rules-of-hooks
             useQuery({
-                queryKey: ['book', bookId, 'characters'],
-                queryFn: () => api.list_all_characters(bookId)
+                queryKey: ['book', book.id, 'characters'],
+                queryFn: () => api.list_all_characters(book.id)
             }),
         [api]
     )
