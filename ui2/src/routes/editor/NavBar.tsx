db05b30c72770de13f00fc2c2fe56dd3d63572ca
devdave <devdave@ominian.net>
2023-11-29 20:07:03 -0700

    flesh out the nav bar more

--- ui2/src/routes/editor/NavBar.tsx
+++ ui2/src/routes/editor/NavBar.tsx
@@ -1,8 +1,8 @@
 import {NavLink as RoutedLink, useParams} from "react-router-dom";
-import {Box, Button, Center, Group, NavLink, SegmentedControl, Title} from "@mantine/core";
+import {Box, Button, Center, Divider, Group, NavLink, SegmentedControl} from "@mantine/core";
 import {Book, Chapter, Scene} from "@src/types.ts";
 import {AppShell} from "@mantine/core";
-import {IconArticle, IconBook, IconList, IconPlus} from "@tabler/icons-react";
+import {IconArticle, IconBook, IconFlagFilled, IconList, IconPlus, IconUsers} from "@tabler/icons-react";
 import {useState} from "react";
 
 
@@ -14,17 +14,15 @@ export const NavBar:React.FC<NavBarParams> = ({book}) => {
 
     const [editMode, setEditMode] = useState("Detailed")
 
-    const {chapter_id} = useParams<{chapter_id?:string}>()
+    const params = useParams<{chapter_id?:string}>()
 
-
-
-
+    console.log("Params", params)
 
     return (
         <AppShell.Navbar>
             <Group>
                 <SegmentedControl
-                    disabled={!chapter_id}
+
                     value={editMode}
                     onChange={setEditMode}
                     data={[
@@ -50,13 +48,43 @@ export const NavBar:React.FC<NavBarParams> = ({book}) => {
                 />
             </Group>
             <Button><IconPlus />Create new chapter</Button>
-            <NavLink label={<><IconBook/><Title>Book: {book.title}</Title></>} component={RoutedLink} to={`/book/${book.id}`}/>
+            <NavLink leftSection={<IconBook/>} label={`Book: ${book.title}`} component={RoutedLink} to={`/book/${book.id}`}/>
+            <NavLink
+                childrenOffset={1}
+                label='Characters'
+                component={RoutedLink}
+                to={`/book/${book.id}/characters`}
+
+
+                opened
+                leftSection={
+                    <Center>
+                        <IconUsers />
+                    </Center>
+                }
+            />
+            <NavLink
+                childrenOffset={1}
+                label='Statuses'
+                component={RoutedLink}
+                to={`/book/${book.id}/statuses`}
+                leftSection={
+                    <Center>
+                        <IconFlagFilled />
+                    </Center>
+                }
+            />
+            <Divider
+                label={"Chapters"}
+            />
 
             { book.chapters.map((chapter:Chapter, idx)=>
                 <NavLink label={`${idx+1}. Chapter: ${chapter.title}`} component={RoutedLink} to={`/book/${book.id}/chapter/${chapter.id}`}>
 
                     {chapter.scenes.map((scene:Scene)=>
-                        <NavLink label={scene.title} component={RoutedLink} to={`/book/${book.id}/chapter/${chapter.id}#${scene.id}`}>
+                        <NavLink label={scene.title} component={RoutedLink} to={`/book/${book.id}/chapter/${chapter.id}#${scene.id}`}
+                            rightSection={<IconFlagFilled style={{color:scene.status.color}}/>}
+                        >
 
                         </NavLink>
                     )}
