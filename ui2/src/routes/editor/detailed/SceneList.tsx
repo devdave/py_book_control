ba4004aade503880bab1a17bc258a3bd236f6e06
devdave <devdave@ominian.net>
2023-12-07 08:21:07 -0700

    Add a handler to remember the current scene

--- ui2/src/routes/editor/detailed/SceneList.tsx
+++ ui2/src/routes/editor/detailed/SceneList.tsx
@@ -1,19 +1,30 @@
-import {useParams} from "react-router-dom";
+import {useLocation, useParams} from "react-router-dom";
 import {useAppContext} from "@src/App.context.ts";
-import {Scene, UniqueId} from "@src/types.ts";
-import {LoadingOverlay} from "@mantine/core";
+import {type Scene, type UniqueId} from "@src/types.ts";
+import {Accordion, LoadingOverlay, Text} from "@mantine/core";
 import {SceneElement} from "@src/routes/editor/detailed/SceneElement.tsx";
+import {useEffect, useState} from "react";
 
 
 export const SceneList = () => {
 
 
 
-    const params = useParams<{book_id:UniqueId, chapter_id:UniqueId}>()
+    const params = useParams<{book_id:UniqueId, chapter_id:UniqueId, mode:string|undefined}>()
+
+    const {hash} = useLocation()
 
     const { chapterBroker} = useAppContext()
 
     const {data:chapter, isLoading:chapterIsLoading} = chapterBroker.fetch(params.book_id as string, params.chapter_id as string)
+
+    const [currentScene, setCurrentScene] = useState(hash?hash.slice(1):null)
+
+
+
+    useEffect(() => {
+        setCurrentScene(hash.slice(1))
+    }, [hash]);
 
 
 
@@ -25,15 +36,22 @@ export const SceneList = () => {
 
     return (
         <>
-            <pre>{JSON.stringify(chapter, null, 4)}</pre>
-            {chapter?.scenes.map((scene:Scene)=>(
-                <div key={`${scene.id}_${scene.updated_on}`} id={scene.id}>
-                    <br/>
-                    <br/>
-                    <br/>
-                    <SceneElement scene={scene}/>
-                </div>
-            ))}
+            {params.mode}
+            <details>
+                <summary>JSON</summary>
+                <pre>{JSON.stringify(chapter, null, 4)}</pre>
+            </details>
+
+            <Accordion value={currentScene} onChange={setCurrentScene}>
+                {chapter?.scenes.map((scene:Scene, idx)=>(
+                    <Accordion.Item key={scene.id} value={scene.id}>
+                        <Accordion.Control><Text>Scene #{idx+1} "{scene.title}"</Text></Accordion.Control>
+                        <Accordion.Panel id={scene.id} >
+                            <SceneElement scene={scene}/>
+                        </Accordion.Panel>
+                    </Accordion.Item>
+                ))}
+            </Accordion>
 
 
         </>
