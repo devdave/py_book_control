9d5c8851035094131c3d8723ceba14ade02ff475
devdave <devdave@ominian.net>
2023-12-28 12:03:58 -0700

    Refactor how settings work

---
+++ lib/app_settings.py
@@ -0,0 +1,14 @@
+from typing import TypedDict
+
+
+class AppSettingValues(TypedDict):
+    fontName: str
+    fontWeight: int
+    fontSize: int
+    lineHeight: int
+    debounceTime: int
+    dontAskOnSplit: bool
+    dontAskOnClear2Delete: bool
+    defaultSceneStatus: str
+    lastImportedPath: str
+    save2Disk: bool
