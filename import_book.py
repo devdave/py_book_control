859b61fd9734d1e8a9431a00c0e7e9b43206aa89
devdave <devdave@ominian.net>
2023-12-28 12:04:12 -0700

    Update import_book.py

--- import_book.py
+++ import_book.py
@@ -2,22 +2,20 @@ import tap
 import pathlib
 
 
-
-
-
 class MainArgs(tap.Tap):
     """
-        Book controll Docx directory importer
+    Book control Docx directory importer
     """
 
-    target_dir:pathlib.Path # Target book directory
+    target_dir: pathlib.Path  # Target book directory
 
     def configure(self) -> None:
-        self.add_argument("target_dir", type=pathlib.Path, help="The target director to import")
+        self.add_argument(
+            "target_dir", type=pathlib.Path, help="The target director to import"
+        )
 
 
 def main():
-
     args = MainArgs().parse_args()
 
     print(f"Will import from {args.target_dir} with title `{args.target_dir.name}`")
@@ -25,24 +23,24 @@ def main():
     targets = []
 
     for element in args.target_dir.iterdir():
-        if element.is_file() is False: continue
-        if element.name.startswith("~"): continue
-        if element.name.startswith("_"): continue
-        if element.suffix != '.docx': continue
+        if any(
+            [
+                element.is_file() is False,
+                element.name.startswith("~"),
+                element.name.startswith("_"),
+                element.suffix != ".docx",
+            ]
+        ):
+            continue
 
         print(f"Would import {element.name}")
         targets.append(element)
 
-    reordered = sorted(targets, key= lambda elm: elm.name.lower())
+    reordered = sorted(targets, key=lambda elm: elm.name.lower())
 
     for target in reordered:
         print(target)
 
 
-
-
-
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
